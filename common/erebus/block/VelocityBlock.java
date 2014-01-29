7accb58c4360d2edf37243dae176a312b5c8deec
ganymedes01 <foka_12@hotmail.com>
2014-01-29 16:14:21 +0000

    Finished Velocity Block
    might be too fast…

--- common/erebus/block/VelocityBlock.java
+++ common/erebus/block/VelocityBlock.java
@@ -5,6 +5,7 @@ import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.util.Icon;
@@ -29,7 +30,23 @@ public class VelocityBlock extends Block {
 
 	@Override
 	public void onEntityCollidedWithBlock(World world, int x, int y, int z, Entity entity) {
-		// TODO do things
+		if (entity instanceof EntityPlayer) {
+			double speed = 0.5D;
+			int meta = world.getBlockMetadata(x, y, z);
+			int[] factorX = { 0, 1, 0, -1 };
+			int[] factorZ = { -1, 0, 1, 0 };
+
+			if (entity != null && entity.posY > y + 0.5D) {
+				if (factorX[meta] == 0 && Math.abs(x + 0.5D - entity.posX) < 0.5D && Math.abs(x + 0.5D - entity.posX) > 0.1D)
+					entity.motionX += Math.signum(x + 0.5D - entity.posX) * Math.min(speed, Math.abs(x + 0.5D - entity.posX)) / 1.2D;
+
+				if (factorZ[meta] == 0 && Math.abs(z + 0.5D - entity.posZ) < 0.5D && Math.abs(z + 0.5D - entity.posZ) > 0.1D)
+					entity.motionZ += Math.signum(z + 0.5D - entity.posZ) * Math.min(speed, Math.abs(z + 0.5D - entity.posZ)) / 1.2D;
+
+				entity.motionX += factorX[meta] * speed;
+				entity.motionZ += factorZ[meta] * speed;
+			}
+		}
 	}
 
 	@Override
