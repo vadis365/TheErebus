e755375fcef9aeae7f0a34b8da9c050d70758568
Vadis365 <curve_linear@hotmail.com>
2014-01-31 22:21:58 +0000

    clean....sigh

--- common/erebus/network/packet/PacketBeetleRamAttack.java
+++ common/erebus/network/packet/PacketBeetleRamAttack.java
@@ -1,14 +1,11 @@
 package erebus.network.packet;
 
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.network.INetworkManager;
 import net.minecraft.network.packet.Packet250CustomPayload;
 
 import com.google.common.io.ByteArrayDataInput;
 
-import erebus.ModItems;
 import erebus.entity.EntityRhinoBeetle;
 import erebus.network.IPacket;
 
