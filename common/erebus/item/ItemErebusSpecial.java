e913878ca8f1c3599875168e0d7786cf9359a656
Vadis365 <curve_linear@hotmail.com>
2014-02-01 20:55:11 +0000

    clean imports

--- common/erebus/item/ItemErebusSpecial.java
+++ common/erebus/item/ItemErebusSpecial.java
@@ -2,24 +2,15 @@ package erebus.item;
 
 import java.util.List;
 
-import net.minecraft.block.Block;
 import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.potion.Potion;
-import net.minecraft.potion.PotionEffect;
 import net.minecraft.util.Icon;
 import net.minecraft.world.World;
-import net.minecraftforge.common.ForgeDirection;
-import cpw.mods.fml.common.network.PacketDispatcher;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
-import erebus.ModBlocks;
-import erebus.block.BlockBambooShoot;
-import erebus.network.PacketHandler;
-import erebus.network.packet.PacketSound;
 
 public class ItemErebusSpecial extends Item {
 
