ab8c95d5297cdeecdf00da59ca0be802b327d2b2
Vadis365 <curve_linear@hotmail.com>
2014-02-09 00:22:55 +0000

    Block Extractor works on block hardness(ish)

--- common/erebus/item/ItemBlockExtractor.java
+++ common/erebus/item/ItemBlockExtractor.java
@@ -15,6 +15,12 @@ import erebus.entity.EntityExtractedBlock;
 
 public class ItemBlockExtractor extends Item {
 	private final Minecraft mc = Minecraft.getMinecraft();
+	public Block block;
+	public int blockID;
+	public int blockMeta;
+	public int objectX, objectY, objectZ;
+	public float blockHardness;
+
 	public ItemBlockExtractor(int id) {
 		super(id);
 		maxStackSize = 1;
@@ -25,47 +31,59 @@ public class ItemBlockExtractor extends Item {
 	public EnumAction getItemUseAction(ItemStack is) {
 		return EnumAction.bow;
 	}
-	
 
 	@Override
 	public int getMaxItemUseDuration(ItemStack is) {
-		return 40;
+		float x = blockHardness * 30;
+		if (x >= 150)
+			x = 150;
+		return (int) x;
 	}
 
 	@Override
 	public ItemStack onItemRightClick(ItemStack is, World world, EntityPlayer player) {
+		MovingObjectPosition objectMouseOver = mc.thePlayer.rayTrace(16, 1);// Distance is 16 atm;
+		if (objectMouseOver != null && objectMouseOver.typeOfHit == EnumMovingObjectType.TILE) {
+			objectX = objectMouseOver.blockX;
+			objectY = objectMouseOver.blockY;
+			objectZ = objectMouseOver.blockZ;
+			blockID = world.getBlockId(objectX, objectY, objectZ);
+			blockMeta = world.getBlockMetadata(objectX, objectY, objectZ);
+			block = Block.blocksList[blockID];
+			if(block !=null)
+				blockHardness = block.getBlockHardness(world, objectX, objectY, objectZ);
+		}
 		player.setItemInUse(is, getMaxItemUseDuration(is));
 		return is;
 	}
 
 	@Override
 	public ItemStack onEaten(ItemStack is, World world, EntityPlayer player) {
-		
-		//world.playSoundAtEntity(player, "erebus:someSoundHere", 1.0F, 1.0F);
+		// world.playSoundAtEntity(player, "erebus:someSoundHere", 1.0F, 1.0F);
+		if(block !=null)
 		extractBlock(is, world, player);
 		is.damageItem(1, player);
 		return is;
 	}
+	
+	@Override
+	  public void onPlayerStoppedUsing(ItemStack is, World world, EntityPlayer player, int count) {
+		  block = null;
+	    }
 
 	protected void extractBlock(ItemStack is, World world, EntityPlayer player) {
-		MovingObjectPosition objectMouseOver = mc.thePlayer.rayTrace(16,1);// Distance is 16 atm;
-			if(objectMouseOver != null && objectMouseOver.typeOfHit == EnumMovingObjectType.TILE) {
-		        int blockID = world.getBlockId(objectMouseOver.blockX, objectMouseOver.blockY, objectMouseOver.blockZ);
-		        int blockMeta = world.getBlockMetadata(objectMouseOver.blockX, objectMouseOver.blockY, objectMouseOver.blockZ);
-		        Block block = Block.blocksList[blockID];
-		        if (!world.isRemote && block != null && canExtract(block)) {
-		        		EntityExtractedBlock entityExtractedBlock;
-		        		entityExtractedBlock = new EntityExtractedBlock(world);
-		        		world.setBlock(objectMouseOver.blockX,objectMouseOver.blockY,objectMouseOver.blockZ, 0);
-						entityExtractedBlock.setLocationAndAngles((double) objectMouseOver.blockX + 0.5F, objectMouseOver.blockY, (double) objectMouseOver.blockZ + 0.5F, 0.0F, 0.0F);
-						entityExtractedBlock.setBlock(blockID, blockMeta);
-						entityExtractedBlock.setHeading(player.posX, player.posY, player.posZ);
-						world.spawnEntityInWorld(entityExtractedBlock);
-			}
+		if (!world.isRemote && block != null && canExtract(block)) {
+			EntityExtractedBlock entityExtractedBlock;
+			entityExtractedBlock = new EntityExtractedBlock(world);
+			world.setBlock(objectX, objectY, objectZ, 0);
+			entityExtractedBlock.setLocationAndAngles((double) objectX + 0.5F, objectY, (double) objectZ + 0.5F, 0.0F, 0.0F);
+			entityExtractedBlock.setBlock(blockID, blockMeta);
+			entityExtractedBlock.setHeading(player.posX, player.posY, player.posZ);
+			world.spawnEntityInWorld(entityExtractedBlock);
 		}
 	}
-	
+
 	private boolean canExtract(Block block) {
-		return  !(block instanceof BlockContainer) &&!(block instanceof BlockPane) && block.blockHardness >= 0 && block.getBlockBoundsMaxX() - block.getBlockBoundsMinX() >= 0.7F && block.getBlockBoundsMaxZ() - block.getBlockBoundsMinZ() >= 0.7F && block.getBlockBoundsMaxY() - block.getBlockBoundsMinY() >= 0.7F;
+		return !(block instanceof BlockContainer) && !(block instanceof BlockPane) && block.blockHardness >= 0 && block.getBlockBoundsMaxX() - block.getBlockBoundsMinX() >= 0.7F && block.getBlockBoundsMaxZ() - block.getBlockBoundsMinZ() >= 0.7F && block.getBlockBoundsMaxY() - block.getBlockBoundsMinY() >= 0.7F;
 	}
 }
