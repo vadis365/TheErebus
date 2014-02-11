119ffa83199f4bc86553097da46058060d754bb7
ganymedes01 <foka_12@hotmail.com>
2014-02-11 15:55:14 +0000

    It works I think...

--- common/erebus/item/ItemBlockExtractor.java
+++ common/erebus/item/ItemBlockExtractor.java
@@ -7,18 +7,12 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.EnumAction;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.Vec3;
 import net.minecraft.world.World;
 import erebus.entity.EntityExtractedBlock;
 
 public class ItemBlockExtractor extends Item {
-
-	public Block block;
-	public int blockID;
-	public int blockMeta;
-	public double targetX, targetY, targetZ;
-	public float blockHardness;
-	private int range;
 
 	public ItemBlockExtractor(int id) {
 		super(id);
@@ -27,82 +21,100 @@ public class ItemBlockExtractor extends Item {
 	}
 
 	@Override
-	public EnumAction getItemUseAction(ItemStack is) {
+	public EnumAction getItemUseAction(ItemStack stack) {
 		return EnumAction.bow;
 	}
 
 	@Override
-	public int getMaxItemUseDuration(ItemStack is) {
-		float x = blockHardness * 30;
+	public int getMaxItemUseDuration(ItemStack stack) {
+		float x = hasTag(stack) ? stack.getTagCompound().getFloat("blockHardness") * 30 : 0;
 		if (x >= 150)
 			x = 150;
 		return (int) x;
 	}
 
 	@Override
-	public ItemStack onItemRightClick(ItemStack is, World world, EntityPlayer player) {
-		getBlockInfo(world, player);
-		player.setItemInUse(is, getMaxItemUseDuration(is));
-		return is;
+	public ItemStack onItemRightClick(ItemStack stack, World world, EntityPlayer player) {
+		getBlockInfo(world, player, stack);
+		player.setItemInUse(stack, getMaxItemUseDuration(stack));
+		return stack;
 	}
 
-	public void getBlockInfo(World world, EntityPlayer player) {
-		if (!world.isRemote) {
-		Vec3 vec3 = player.getLookVec().normalize();
-		targetX = player.posX;
-		targetY = player.posY + player.getEyeHeight() - 0.10000000149011612D;
-		targetZ = player.posZ;
-		
-		while (world.isAirBlock((int) targetX, (int) targetY, (int) targetZ) && range <= 15) { // range of 16
-			range++;
-			targetX += vec3.xCoord;
-			targetY += vec3.yCoord;
-			targetZ += vec3.zCoord;
-			if (!world.isAirBlock((int) targetX, (int) targetY, (int) targetZ)) {
-				System.out.println("found Block"+range);
-				blockID = world.getBlockId((int)targetX, (int)targetY, (int)targetZ);
-				blockMeta = world.getBlockMetadata((int)targetX, (int)targetY, (int)targetZ);
-				block = Block.blocksList[blockID];
-				if (block != null)
-					blockHardness = block.getBlockHardness(world, (int)targetX, (int)targetY, (int)targetZ);		
+	public void getBlockInfo(World world, EntityPlayer player, ItemStack stack) {
+		if (!world.isRemote && hasTag(stack)) {
+			Vec3 vec3 = player.getLookVec().normalize();
+			double targetX = player.posX;
+			double targetY = player.posY + player.getEyeHeight() - 0.10000000149011612D;
+			double targetZ = player.posZ;
+
+			while (world.isAirBlock((int) targetX, (int) targetY, (int) targetZ) && stack.getTagCompound().getInteger("range") <= 15) { // range of 16
+				stack.getTagCompound().setInteger("range", stack.getTagCompound().getInteger("range") + 1);
+				targetX += vec3.xCoord;
+				targetY += vec3.yCoord;
+				targetZ += vec3.zCoord;
+				if (!world.isAirBlock((int) targetX, (int) targetY, (int) targetZ)) {
+					System.out.println("found Block" + stack.getTagCompound().getInteger("range"));
+
+					stack.getTagCompound().setInteger("blockID", world.getBlockId((int) targetX, (int) targetY, (int) targetZ));
+					stack.getTagCompound().setInteger("blockMeta", world.getBlockMetadata((int) targetX, (int) targetY, (int) targetZ));
+					Block block = Block.blocksList[stack.getTagCompound().getInteger("blockID")];
+					if (block != null) {
+						stack.getTagCompound().setFloat("blockHardness", block.getBlockHardness(world, (int) targetX, (int) targetY, (int) targetZ));
+						stack.getTagCompound().setInteger("targetX", (int) targetX);
+						stack.getTagCompound().setInteger("targetY", (int) targetY);
+						stack.getTagCompound().setInteger("targetZ", (int) targetZ);
+					}
+				}
 			}
 		}
 	}
+
+	@Override
+	public ItemStack onEaten(ItemStack stack, World world, EntityPlayer player) {
+		if (!world.isRemote && hasTag(stack)) {
+			Block block = Block.blocksList[stack.getTagCompound().getInteger("blockID")];
+			if (block != null && canExtract(block))
+				extractBlock(stack, world, player);
+		}
+		stack.damageItem(1, player);
+		return stack;
 	}
 
 	@Override
-	public ItemStack onEaten(ItemStack is, World world, EntityPlayer player) {
-		// world.playSoundAtEntity(player, "erebus:someSoundHere", 1.0F, 1.0F);
-		if (!world.isRemote && block != null && canExtract(block))
-			extractBlock(is, world, player);
-		is.damageItem(1, player);
-		return is;
+	public void onPlayerStoppedUsing(ItemStack stack, World world, EntityPlayer player, int count) {
+		resetStats(stack);
 	}
 
-	@Override
-	public void onPlayerStoppedUsing(ItemStack is, World world, EntityPlayer player, int count) {
-		resetStats();
-	}
-
-	protected void extractBlock(ItemStack is, World world, EntityPlayer player) {
+	protected void extractBlock(ItemStack stack, World world, EntityPlayer player) {
 		if (!world.isRemote) {
 			EntityExtractedBlock entityExtractedBlock;
 			entityExtractedBlock = new EntityExtractedBlock(world);
-			world.setBlock((int)targetX, (int)targetY, (int)targetZ, 0);
-			entityExtractedBlock.setLocationAndAngles((int)targetX + 0.5D, (int)targetY, (int)targetZ + 0.5D, 0.0F, 0.0F);
-			entityExtractedBlock.setBlock(blockID, blockMeta);
+			int targetX = stack.getTagCompound().getInteger("targetX");
+			int targetY = stack.getTagCompound().getInteger("targetY");
+			int targetZ = stack.getTagCompound().getInteger("targetZ");
+
+			world.setBlock(targetX, targetY, targetZ, 0);
+			entityExtractedBlock.setLocationAndAngles(targetX + 0.5D, targetY, targetZ + 0.5D, 0.0F, 0.0F);
+			entityExtractedBlock.setBlock(stack.getTagCompound().getInteger("blockID"), stack.getTagCompound().getInteger("blockMeta"));
 			entityExtractedBlock.setHeading(player.posX, player.posY, player.posZ);
 			world.spawnEntityInWorld(entityExtractedBlock);
+			resetStats(stack);
 		}
-		resetStats();
 	}
-	
-	public void resetStats() {
-		block = null;
-		range = 0;
+
+	public void resetStats(ItemStack stack) {
+		stack.getTagCompound().setInteger("range", 0);
 	}
 
 	private boolean canExtract(Block block) {
 		return !(block instanceof BlockContainer) && !(block instanceof BlockPane) && block.blockHardness >= 0 && block.getBlockBoundsMaxX() - block.getBlockBoundsMinX() >= 0.7F && block.getBlockBoundsMaxZ() - block.getBlockBoundsMinZ() >= 0.7F && block.getBlockBoundsMaxY() - block.getBlockBoundsMinY() >= 0.7F;
+	}
+
+	private boolean hasTag(ItemStack stack) {
+		if (!stack.hasTagCompound()) {
+			stack.setTagCompound(new NBTTagCompound());
+			return false;
+		}
+		return true;
 	}
 }
