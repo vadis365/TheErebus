8699782aac7997ad29264b337f0fa85c644e4a54
Vadis365 <curve_linear@hotmail.com>
2014-03-27 00:43:01 +0000

    Messing about and made a house spawn

--- common/erebus/world/biomes/BiomeFungalForest.java
+++ common/erebus/world/biomes/BiomeFungalForest.java
@@ -1,6 +1,11 @@
 package erebus.world.biomes;
 import java.util.Random;
+
+import net.minecraft.block.Block;
 import net.minecraft.world.World;
+import net.minecraft.world.gen.feature.WorldGenBigMushroom;
+import net.minecraft.world.gen.feature.WorldGenFlowers;
+import erebus.world.feature.structure.WorldGenMushroomHouse;
 
 // @formatter:off
 public class BiomeFungalForest extends BiomeBaseErebus{
@@ -14,6 +19,35 @@ public class BiomeFungalForest extends BiomeBaseErebus{
 	}
 	
 	@Override
-	public void generateBiomeFeatures(World world, Random rand, int x, int z){}
+	public void generateBiomeFeatures(World world, Random rand, int x, int z){
+		
+		for(int attempt=0; attempt<200; attempt++){
+			int yy=15+rand.nextInt(90);
+			new WorldGenMushroomHouse().generate(world,rand,x,yy,z);
+		}
+		
+		new WorldGenFlowers(Block.mushroomBrown.blockID).generate(world,rand,x+getRandomXZOffset(rand),rand.nextInt(128),z+getRandomXZOffset(rand));
+		new WorldGenFlowers(Block.mushroomRed.blockID).generate(world,rand,x+getRandomXZOffset(rand),rand.nextInt(128),z+getRandomXZOffset(rand));
+
+		for(int attempt=0; attempt<12; attempt++){
+			int xx=x+getRandomXZOffset(rand),
+				yy=15+rand.nextInt(90),
+				zz=z+getRandomXZOffset(rand);
+			
+			if (world.isAirBlock(xx,yy,zz) && world.getBlockId(xx,yy-1,zz)==Block.grass.blockID){
+				new WorldGenBigMushroom(0).generate(world,rand,xx,yy,zz);
+			}
+		}
+
+		for(int attempt=0; attempt<20; attempt++){
+			int xx=x+getRandomXZOffset(rand),
+				yy=15+rand.nextInt(90),
+				zz=z+getRandomXZOffset(rand);
+			
+			if (world.isAirBlock(xx,yy,zz) && world.getBlockId(xx,yy-1,zz)==Block.grass.blockID){
+				new WorldGenBigMushroom(1).generate(world,rand,xx,yy,zz);
+			}
+		}
+	}
 }
 //@formatter:on
---
+++ common/erebus/world/feature/structure/WorldGenMushroomHouse.java
@@ -0,0 +1,82 @@
+package erebus.world.feature.structure;
+
+import java.util.Random;
+
+import net.minecraft.block.Block;
+import net.minecraft.entity.passive.EntityVillager;
+import net.minecraft.world.World;
+import net.minecraft.world.gen.feature.WorldGenerator;
+
+// @formatter:off
+public class WorldGenMushroomHouse extends WorldGenerator {
+	int airX, airZ, doorX, doorZ;
+
+	@Override
+	public boolean generate(World world, Random rand, int x, int y, int z) {
+
+		for (int x1 = x - 6; x1 <= x + 6; x1++)
+			for (int z1 = z - 6; z1 <= z + 6; z1++)
+				for (int y1 = y + 1; y1 < y + 6; y1++) {
+					if (!world.isAirBlock(x1, y1, z1))
+						return false;
+				}
+
+		for (int x1 = x - 6; x1 <= x + 6; x1++)
+			for (int z1 = z - 6; z1 <= z + 6; z1++) {
+				if (world.getBlockId(x1, y, z1) != Block.grass.blockID)
+					return false;
+			}
+
+		for (int x1 = x - 6; x1 <= x + 6; x1++)
+			for (int z1 = z - 6; z1 <= z + 6; z1++)
+				for (int y1 = y; y1 < y + 6; y1++) {
+					double dSq = Math.pow(x1 - x, 2.0D)
+							+ Math.pow(z1 - z, 2.0D) + Math.pow(y1 - y, 2.0D);
+					if (Math.round(Math.sqrt(dSq)) < 6)
+						if ((dSq >= Math.pow(6 - 2, 2.0D)) || (y1 == y))
+							world.setBlock(x1, y1, z1,
+									Block.mushroomCapRed.blockID, 5, 3);
+						else
+							world.setBlock(x1, y1, z1, 0);
+				}
+		
+		int direction = rand.nextInt(4);
+
+		if (direction == 0) {
+			airX = 5;
+			airZ = 0;
+			doorX = 4;
+			doorZ = 0;
+		}
+		if (direction == 1) {
+			airX = 0;
+			airZ = 5;
+			doorX = 0;
+			doorZ = 4;
+		}
+		if (direction == 2) {
+			airX = -5;
+			airZ = 0;
+			doorX = -4;
+			doorZ = 0;
+		}
+		if (direction == 3) {
+			airX = 0;
+			airZ = -5;
+			doorX = 0;
+			doorZ = -4;
+		}
+		
+		world.setBlock(x + airX, y + 1, z + airZ, 0);
+		world.setBlock(x + airX, y + 2, z + airZ, 0);
+		world.setBlock(x + doorX, y + 1, z + doorZ, Block.doorWood.blockID, direction, 3);
+		world.setBlock(x + doorX, y + 2, z + doorZ, Block.doorWood.blockID, direction + 8, 3);
+		
+		EntityVillager villager =new EntityVillager(world);
+		villager.setLocationAndAngles(x,y+1,z,rand.nextFloat()*360F,0F);
+		villager.forceSpawn=true;
+		world.spawnEntityInWorld(villager);
+		return true;
+	}
+}
+// @formatter:on
