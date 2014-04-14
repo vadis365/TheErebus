0f15f63829685f98f344c586399d164beaaaa160
Vadis365 <curve_linear@hotmail.com>
2014-04-14 01:08:51 +0100

    Made proper glowing jar rendering

--- common/erebus/client/model/block/ModelGlowingJar.java
+++ common/erebus/client/model/block/ModelGlowingJar.java
@@ -2,18 +2,16 @@ package erebus.client.model.block;
 
 import net.minecraft.client.model.ModelBase;
 import net.minecraft.client.model.ModelRenderer;
-import net.minecraft.tileentity.TileEntity;
 
 import org.lwjgl.opengl.GL11;
 
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
-import erebus.tileentity.TileEntityJarOHoney;
 
 @SideOnly(Side.CLIENT)
 public class ModelGlowingJar extends ModelBase {
 
-	private final ModelRenderer jar, lid, neck, honey;
+	private final ModelRenderer jar, lid, neck;
 
 	public ModelGlowingJar() {
 		textureWidth = 128;
@@ -30,26 +28,10 @@ public class ModelGlowingJar extends ModelBase {
 		neck.addBox(-7F, 0F, -7F, 14, 12, 14);
 		neck.setRotationPoint(0F, 12F, 0F);
 		neck.setTextureSize(128, 64);
-		honey = new ModelRenderer(this, 0, 27);
-		honey.addBox(-6F, -1F, -6F, 12, 1, 12);
-		honey.setRotationPoint(0F, 12F, 0F);
-		honey.setTextureSize(128, 64);
 	}
 
 	@SideOnly(Side.CLIENT)
-	public void render(TileEntity tile) {
-		if (tile instanceof TileEntityJarOHoney) {
-			int amount = ((TileEntityJarOHoney)tile).tank.getFluidAmount();
-			int capacity = ((TileEntityJarOHoney)tile).tank.getCapacity();
-			System.out.println("Fluid: "+amount);
-			System.out.println("Capacity: "+capacity);
-			float size = 12F/capacity*amount;
-			GL11.glPushMatrix();
-			GL11.glTranslatef(0F, 9.75F, 0F);
-			GL11.glScalef(1F, -size, -1F);
-			jar.render(0.0625F);
-			GL11.glPopMatrix();
-		}
+	public void render() {
 		lid.render(0.0625F);
 		GL11.glPushMatrix();
 		GL11.glEnable(3042);
--- common/erebus/client/render/tileentity/TileEntityGlowingJarRenderer.java
+++ common/erebus/client/render/tileentity/TileEntityGlowingJarRenderer.java
@@ -1,5 +1,6 @@
 package erebus.client.render.tileentity;
 
+import net.minecraft.client.renderer.RenderBlocks;
 import net.minecraft.client.renderer.entity.RenderItem;
 import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
@@ -12,9 +13,9 @@ import org.lwjgl.opengl.GL11;
 
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
+import erebus.ModBlocks;
 import erebus.ModItems;
 import erebus.client.model.block.ModelGlowingJar;
-import erebus.tileentity.TileEntityGlowingJar;
 import erebus.tileentity.TileEntityJarOHoney;
 
 @SideOnly(Side.CLIENT)
@@ -23,7 +24,8 @@ public class TileEntityGlowingJarRenderer extends TileEntitySpecialRenderer {
 	private final ModelGlowingJar glowingJar = new ModelGlowingJar();
 	private final RenderItem renderItem;
 	private final ItemStack glowThingy = new ItemStack(ModItems.erebusMaterials, 1, 13);
-
+	private final RenderBlocks blockRenderer = new RenderBlocks();
+	
 	public TileEntityGlowingJarRenderer() {
 		renderItem = new RenderItem() {
 			@Override
@@ -37,7 +39,17 @@ public class TileEntityGlowingJarRenderer extends TileEntitySpecialRenderer {
 	@Override
 	public void renderTileEntityAt(TileEntity tile, double x, double y, double z, float partialTickTime) {
 		if (tile instanceof TileEntityJarOHoney) {
-			// TODO render honey or whatever
+			int amount = ((TileEntityJarOHoney)tile).tank.getFluidAmount();
+			int capacity = ((TileEntityJarOHoney)tile).tank.getCapacity();
+			float size = 0.70F / capacity * amount;
+			if (amount >= 100) {
+			GL11.glPushMatrix();
+			GL11.glTranslated((float) x + 0.5F, (float) (y + 0.030F + size * 0.5F), (float) z + 0.5F);
+			GL11.glScalef(0.55F, -size, -0.55F);
+			bindTexture(new ResourceLocation("erebus:textures/blocks/blockAmber.png"));
+			blockRenderer.renderBlockAsItem(ModBlocks.blockAmber, 0, 1.0F);
+			GL11.glPopMatrix();
+			}
 		} else {
 			EntityItem ghostEntityItem = new EntityItem(tile.worldObj);
 			ghostEntityItem.hoverStart = 0.0F;
@@ -56,7 +68,7 @@ public class TileEntityGlowingJarRenderer extends TileEntitySpecialRenderer {
 		GL11.glTranslatef((float) x + 0.5F, (float) y + 1.51F, (float) z + 0.5F);
 		GL11.glScalef(0.7F, -1F, -0.7F);
 		GL11.glDisable(GL11.GL_CULL_FACE);
-		glowingJar.render(tile);
+		glowingJar.render();
 		GL11.glEnable(GL11.GL_CULL_FACE);
 		GL11.glPopMatrix();
 	}
