c2a9cf3145e94ddc121668d944a813eacc803a9b
ganymedes01 <foka_12@hotmail.com>
2014-05-04 00:46:18 -0300

    And make block non-collidable. because tiny BBs are annoying :3

--- common/erebus/block/BlockGlowGem.java
+++ common/erebus/block/BlockGlowGem.java
@@ -12,6 +12,7 @@ import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.util.MathHelper;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
@@ -268,6 +269,11 @@ public class BlockGlowGem extends BlockContainer {
 			}
 		world.setBlockMetadataWithNotify(x, y, z, newMeta, 3);
 		System.out.println("META: " + newMeta);
+	}
+
+	@Override
+	public AxisAlignedBB getCollisionBoundingBoxFromPool(World world, int x, int y, int z) {
+		return null;
 	}
 
 	@Override
