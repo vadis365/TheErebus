ca34bad5e6a4a13f32c191a679e0d0ba273a7dd0
ganymedes01 <foka_12@hotmail.com>
2014-05-06 15:11:37 -0300

    Made jars of honey render their owners names
    Kind of. Will fix it later:3

--- common/erebus/client/render/tileentity/TileEntityGlowingJarRenderer.java
+++ common/erebus/client/render/tileentity/TileEntityGlowingJarRenderer.java
@@ -1,6 +1,9 @@
 package erebus.client.render.tileentity;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.renderer.RenderBlocks;
+import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.entity.RenderItem;
 import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
@@ -25,7 +28,7 @@ public class TileEntityGlowingJarRenderer extends TileEntitySpecialRenderer {
 	private final RenderItem renderItem;
 	private final ItemStack glowThingy = new ItemStack(ModItems.erebusMaterials, 1, 13);
 	private final RenderBlocks blockRenderer = new RenderBlocks();
-	
+
 	public TileEntityGlowingJarRenderer() {
 		renderItem = new RenderItem() {
 			@Override
@@ -39,20 +42,21 @@ public class TileEntityGlowingJarRenderer extends TileEntitySpecialRenderer {
 	@Override
 	public void renderTileEntityAt(TileEntity tile, double x, double y, double z, float partialTickTime) {
 		if (tile instanceof TileEntityJarOHoney) {
-			int amount = ((TileEntityJarOHoney)tile).tank.getFluidAmount();
-			int capacity = ((TileEntityJarOHoney)tile).tank.getCapacity();
+			int amount = ((TileEntityJarOHoney) tile).tank.getFluidAmount();
+			int capacity = ((TileEntityJarOHoney) tile).tank.getCapacity();
 			float size = 0.70F / capacity * amount;
 			if (amount >= 100) {
-			GL11.glPushMatrix();
-			GL11.glEnable(3042);
-			GL11.glBlendFunc(770, 771);
-			GL11.glTranslated((float) x + 0.5F, (float) (y + 0.030F + size * 0.5F), (float) z + 0.5F);
-			GL11.glScalef(0.55F, -size, -0.55F);
-			bindTexture(new ResourceLocation("erebus:textures/blocks/blockAmber.png"));
-			blockRenderer.renderBlockAsItem(ModBlocks.blockAmber, 0, 1.0F);
-			GL11.glDisable(3042);
-			GL11.glPopMatrix();
+				GL11.glPushMatrix();
+				GL11.glEnable(3042);
+				GL11.glBlendFunc(770, 771);
+				GL11.glTranslated((float) x + 0.5F, (float) (y + 0.030F + size * 0.5F), (float) z + 0.5F);
+				GL11.glScalef(0.55F, -size, -0.55F);
+				bindTexture(new ResourceLocation("erebus:textures/blocks/blockAmber.png"));
+				blockRenderer.renderBlockAsItem(ModBlocks.blockAmber, 0, 1.0F);
+				GL11.glDisable(3042);
+				GL11.glPopMatrix();
 			}
+			renderNameTag("Boo Boo", x, y, z);
 		} else {
 			EntityItem ghostEntityItem = new EntityItem(tile.worldObj);
 			ghostEntityItem.hoverStart = 0.0F;
@@ -72,6 +76,45 @@ public class TileEntityGlowingJarRenderer extends TileEntitySpecialRenderer {
 		GL11.glDisable(GL11.GL_CULL_FACE);
 		glowingJar.render();
 		GL11.glEnable(GL11.GL_CULL_FACE);
+		GL11.glPopMatrix();
+	}
+
+	private void renderNameTag(String name, double x, double y, double z) {
+		FontRenderer fontrenderer = Minecraft.getMinecraft().fontRenderer;
+		float scale = 0.02666667F;
+		float height = 0.8F;
+
+		GL11.glPushMatrix();
+		GL11.glTranslated(x + 0.5F, y + height + 0.5F, z + 0.5F);
+		GL11.glNormal3f(0.0F, 1.0F, 0.0F);
+		GL11.glRotatef(-RenderManager.instance.playerViewY, 0.0F, 1.0F, 0.0F);
+		GL11.glRotatef(RenderManager.instance.playerViewX, 1.0F, 0.0F, 0.0F);
+		GL11.glScalef(-scale, -scale, scale);
+		GL11.glDisable(GL11.GL_LIGHTING);
+		GL11.glDepthMask(false);
+		GL11.glDisable(GL11.GL_DEPTH_TEST);
+		GL11.glEnable(GL11.GL_BLEND);
+		GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+		Tessellator tessellator = Tessellator.instance;
+		GL11.glDisable(GL11.GL_TEXTURE_2D);
+
+		tessellator.startDrawingQuads();
+		int width = fontrenderer.getStringWidth(name) / 2;
+		tessellator.setColorRGBA_F(0.0F, 0.0F, 0.0F, 0.25F);
+		tessellator.addVertex(-width - 1, -1, 0.0D);
+		tessellator.addVertex(-width - 1, 8, 0.0D);
+		tessellator.addVertex(width + 1, 8, 0.0D);
+		tessellator.addVertex(width + 1, -1, 0.0D);
+		tessellator.draw();
+
+		GL11.glEnable(GL11.GL_TEXTURE_2D);
+		fontrenderer.drawString(name, -fontrenderer.getStringWidth(name) / 2, 0, 553648127);
+		GL11.glEnable(GL11.GL_DEPTH_TEST);
+		GL11.glDepthMask(true);
+		fontrenderer.drawString(name, -fontrenderer.getStringWidth(name) / 2, 0, -1);
+		GL11.glEnable(GL11.GL_LIGHTING);
+		GL11.glDisable(GL11.GL_BLEND);
+		GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
 		GL11.glPopMatrix();
 	}
 }
