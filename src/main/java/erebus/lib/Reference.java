3dc558183ef8b4e7629778dab3afdc532152fdb4
ganymedes01 <foka_12@hotmail.com>
2014-05-21 17:48:26 -0300

    Update dependencies

--- src/main/java/erebus/lib/Reference.java
+++ src/main/java/erebus/lib/Reference.java
@@ -4,7 +4,7 @@ public class Reference {
 	public static final String MOD_ID = "Erebus";
 	public static final String MOD_NAME = "Erebus";
 	public static final String MOD_VERSION = "v0.2.0a";
-	public static final String MOD_DEPENDENCIES = "required-after:Forge@[9.10.1.965,)";
+	public static final String MOD_DEPENDENCIES = "required-after:Forge@[10.12.0.1075,);";
 	public static final String SP_CLIENT = "erebus.core.proxy.ClientProxy";
 	public static final String SP_SERVER = "erebus.core.proxy.CommonProxy";
 	public static final String CHANNEL = "erebus";
