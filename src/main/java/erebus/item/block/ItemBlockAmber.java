990eae67b4165d3a14cf1850e94a589a26d12ebb
ganymedes01 <foka_12@hotmail.com>
2014-05-24 15:45:02 -0300

    if the ItemBlock class extends ItemCloth the block renders transparent
    just minecraft things

--- src/main/java/erebus/item/block/ItemBlockAmber.java
+++ src/main/java/erebus/item/block/ItemBlockAmber.java
@@ -1,10 +1,26 @@
 package erebus.item.block;
 
 import net.minecraft.block.Block;
+import net.minecraft.item.ItemCloth;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.IIcon;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 
-public class ItemBlockAmber extends ItemBlockGeneric {
+public class ItemBlockAmber extends ItemCloth {
 
 	public ItemBlockAmber(Block block) {
-		super(block, "blockAmber");
+		super(block);
+	}
+
+	@Override
+	public String getUnlocalizedName(ItemStack is) {
+		return "blockAmber" + is.getItemDamage();
+	}
+
+	@Override
+	@SideOnly(Side.CLIENT)
+	public IIcon getIconFromDamage(int meta) {
+		return field_150939_a.func_149735_b(2, meta);
 	}
 }
