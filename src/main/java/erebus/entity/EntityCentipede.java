9c19c002d0bf80f99feb39bf5fef6684c50f6253
vadis365 <curve_linear@hotmail.com>
2014-06-12 02:45:23 +0100

    WIP centipede tunnel griefing

--- src/main/java/erebus/entity/EntityCentipede.java
+++ src/main/java/erebus/entity/EntityCentipede.java
@@ -1,6 +1,7 @@
 package erebus.entity;
 
 import net.minecraft.block.Block;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EnumCreatureAttribute;
 import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.ai.EntityAIHurtByTarget;
@@ -10,9 +11,12 @@ import net.minecraft.entity.ai.EntityAIWander;
 import net.minecraft.entity.ai.EntityAIWatchClosest;
 import net.minecraft.entity.monster.EntityMob;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemStack;
 import net.minecraft.potion.Potion;
 import net.minecraft.potion.PotionEffect;
+import net.minecraft.util.DamageSource;
+import net.minecraft.util.MathHelper;
 import net.minecraft.world.EnumDifficulty;
 import net.minecraft.world.World;
 import erebus.ModItems;
@@ -40,7 +44,7 @@ public class EntityCentipede extends EntityMob {
 		super.applyEntityAttributes();
 		getEntityAttribute(SharedMonsterAttributes.movementSpeed).setBaseValue(1.0D); // Movespeed
 		getEntityAttribute(SharedMonsterAttributes.maxHealth).setBaseValue(25.0D); // MaxHealth
-		getEntityAttribute(SharedMonsterAttributes.attackDamage).setBaseValue(getAttackStrength()); // atkDmg
+		getEntityAttribute(SharedMonsterAttributes.attackDamage).setBaseValue(0D); //getAttackStrength()// atkDmg
 		getEntityAttribute(SharedMonsterAttributes.followRange).setBaseValue(16.0D); // followRange
 	}
 
@@ -96,6 +100,10 @@ public class EntityCentipede extends EntityMob {
 	protected void func_145780_a(int x, int y, int z, Block block) {  // playStepSound
 		playSound("erebus:centipedewalk", 0.5F, 1.0F);
 	}
+	
+	protected String getTunnelingSound() {
+		return "erebus:canDigsound";
+	}
 
 	@Override
 	protected float getSoundVolume() {
@@ -130,5 +138,65 @@ public class EntityCentipede extends EntityMob {
 			if (duration > 0)
 				player.addPotionEffect(new PotionEffect(Potion.poison.id, duration * 20, 0));
 		}
+	}
+	
+	public boolean isOnSandOrDirt() {
+		return worldObj.getBlock(MathHelper.floor_double(posX), MathHelper.floor_double(boundingBox.minY) - 1, MathHelper.floor_double(posZ)) == Blocks.sand
+				||worldObj.getBlock(MathHelper.floor_double(posX), MathHelper.floor_double(boundingBox.minY) - 1, MathHelper.floor_double(posZ)) == Blocks.dirt
+				||worldObj.getBlock(MathHelper.floor_double(posX), MathHelper.floor_double(boundingBox.minY) - 1, MathHelper.floor_double(posZ)) == Blocks.grass;
+			
+	}
+
+	@Override
+	public void onUpdate() {
+		super.onUpdate();
+		if (findPlayerToAttack() != null)
+			entityToAttack = findPlayerToAttack();
+		else
+			entityToAttack = null;
+
+		if (!worldObj.isRemote && getEntityToAttack() != null && isOnSandOrDirt())
+			yOffset = -1.0F;
+		 // TODO need to set another flag  just to stop yOffset digging to the Earth's core
+		else
+			yOffset = 0F;
+	}
+
+	@Override
+	protected Entity findPlayerToAttack() {
+		EntityPlayer player = worldObj.getClosestVulnerablePlayerToEntity(this, 16.0D);
+		return player;
+	}
+
+	@Override
+	public boolean attackEntityFrom(DamageSource source, float damage) {
+		if (source.equals(DamageSource.inWall) || source.equals(DamageSource.drown))
+			return false;
+		return super.attackEntityFrom(source, damage);
+	}
+	
+	@Override
+	public void onLivingUpdate() {
+	            if(yOffset ==-1.0F)
+	            	tunnel();
+	            
+	        super.onLivingUpdate();
+	    }
+
+	public boolean canCentipedeDestroy(World world, int x, int y, int z) {
+		return world.getBlock( x, y, z) == Blocks.sand || world.getBlock( x, y, z) == Blocks.dirt|| world.getBlock( x, y, z) == Blocks.grass;		
+	}
+
+	private void tunnel() {
+			boolean canDig = false;
+			Block block = this.worldObj.getBlock(MathHelper.floor_double(posX), MathHelper.floor_double(posY), MathHelper.floor_double(posZ));
+				if (block != null)
+					if (this.canCentipedeDestroy(worldObj, MathHelper.floor_double(posX), MathHelper.floor_double(posY), MathHelper.floor_double(posZ))) {
+						canDig = true;
+						worldObj.setBlock(MathHelper.floor_double(posX), MathHelper.floor_double(posY+1), MathHelper.floor_double(posZ), block);
+	                    worldObj.setBlock(MathHelper.floor_double(posX), MathHelper.floor_double(posY), MathHelper.floor_double(posZ), Blocks.air);
+	                    }
+	    if (canDig)
+	    	this.worldObj.playSoundEffect((int)posX, (int)posY, (int)posZ, getTunnelingSound(), 1.0F, 1.0F);
 	}
 }
