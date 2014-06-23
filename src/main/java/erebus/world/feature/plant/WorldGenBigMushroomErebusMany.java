b7714925c229d3dbf92eb86b055841583ff5e7cc
vadis365 <curve_linear@hotmail.com>
2014-06-23 22:23:00 +0100

    decoded some stuff a bit

--- src/main/java/erebus/world/feature/plant/WorldGenBigMushroomErebusMany.java
+++ src/main/java/erebus/world/feature/plant/WorldGenBigMushroomErebusMany.java
@@ -11,9 +11,9 @@ import erebus.ModBlocks;
 public class WorldGenBigMushroomErebusMany extends WorldGenerator {
 	private int mushroomType = -1;
 
-	public WorldGenBigMushroomErebusMany(int par1) {
+	public WorldGenBigMushroomErebusMany(int type) {
 		super(true);
-		this.mushroomType = par1;
+		this.mushroomType = type;
 	}
 
 	public WorldGenBigMushroomErebusMany() {
@@ -24,29 +24,29 @@ public class WorldGenBigMushroomErebusMany extends WorldGenerator {
 	public boolean generate(World world, Random rand, int x, int y, int z) {
 		int type = this.mushroomType;
 
-		int i1 = rand.nextInt(3) + 4;
+		int height = rand.nextInt(8) + 4;
 		boolean flag = true;
 		Block block;
 
-		if (y >= 1 && y + i1 + 1 < 256) {
-			int j1;
-			int k1;
-			int l1;
-			int i2;
+		if (y >= 1 && y + height + 1 < 256) {
+			int yMax;
+			int yy;
+			int capRad;
+			int xx;
 
-			for (j1 = y; j1 <= y + 1 + i1; ++j1) {
-				byte b0 = 3;
+			for (yMax = y; yMax <= y + 1 + height; ++yMax) {
+				byte rad = 3;
 
-				if (j1 <= y + 3) {
-					b0 = 0;
+				if (yMax <= y + 3) {
+					rad = 0;
 				}
 
-				for (k1 = x - b0; k1 <= x + b0 && flag; ++k1) {
-					for (l1 = z - b0; l1 <= z + b0 && flag; ++l1) {
-						if (j1 >= 0 && j1 < 256) {
-							block = world.getBlock(k1, j1, l1);
+				for (yy = x - rad; yy <= x + rad && flag; ++yy) {
+					for (capRad = z - rad; capRad <= z + rad && flag; ++capRad) {
+						if (yMax >= 0 && yMax < 256) {
+							block = world.getBlock(yy, yMax, capRad);
 
-							if (block.getMaterial() != Material.air && !block.isLeaves(world, k1, j1, l1)) {
+							if (block.getMaterial() != Material.air && !block.isLeaves(world, yy, yMax, capRad)) {
 								flag = false;
 							}
 						} else {
@@ -64,112 +64,112 @@ public class WorldGenBigMushroomErebusMany extends WorldGenerator {
 				if (block != Blocks.dirt && block != Blocks.grass && block != Blocks.mycelium) {
 					return false;
 				} else {
-					int j2 = y + i1;
+					int capLength = y + height;
 
 					if (type == 0) {
-						j2 = y + i1 - 3;
+						capLength = y + height - 3;
 					}
 					
 					if (type == 1) {
-						j2 = y + i1 - 1;
+						capLength = y + height - 1;
 					}
 					
 					if (type == 4) {
-						j2 = y + i1 - 1;
+						capLength = y + height - 1;
 					}
 
-					for (k1 = j2; k1 <= y + i1; ++k1) {
-						l1 = 1;
+					for (yy = capLength; yy <= y + height; ++yy) {
+						capRad = 1;
 
-						if (k1 < y + i1) {
-							++l1;
+						if (yy < y + height) {
+							++capRad;
 						}
 
 						if (type == 2) {
-							l1 = 3;
+							capRad = 3;
 						}
 						
 						if (type == 4) {
-							l1 = 2;
+							capRad = 2;
 						}
 
 
-						for (i2 = x - l1; i2 <= x + l1; ++i2) {
-							for (int k2 = z - l1; k2 <= z + l1; ++k2) {
-								int l2 = 5;
+						for (xx = x - capRad; xx <= x + capRad; ++xx) {
+							for (int zz = z - capRad; zz <= z + capRad; ++zz) {
+								int meta = 5;
 
-								if (i2 == x - l1) {
-									--l2;
+								if (xx == x - capRad) {
+									--meta;
 								}
 
-								if (i2 == x + l1) {
-									++l2;
+								if (xx == x + capRad) {
+									++meta;
 								}
 
-								if (k2 == z - l1) {
-									l2 -= 3;
+								if (zz == z - capRad) {
+									meta -= 3;
 								}
 
-								if (k2 == z + l1) {
-									l2 += 3;
+								if (zz == z + capRad) {
+									meta += 3;
 								}
 
-								if (type == 2 || type == 4 || k1 < y + i1) {
-									if ((i2 == x - l1 || i2 == x + l1) && (k2 == z - l1 || k2 == z + l1)) {
+								if (type == 2 || type == 4 || yy < y + height) {
+									if ((xx == x - capRad || xx == x + capRad) && (zz == z - capRad || zz == z + capRad)) {
 										continue;
 									}
 
-									if (i2 == x - (l1 - 1) && k2 == z - l1) {
-										l2 = 1;
+									if (xx == x - (capRad - 1) && zz == z - capRad) {
+										meta = 1;
 									}
 
-									if (i2 == x - l1 && k2 == z - (l1 - 1)) {
-										l2 = 1;
+									if (xx == x - capRad && zz == z - (capRad - 1)) {
+										meta = 1;
 									}
 
-									if (i2 == x + (l1 - 1) && k2 == z - l1) {
-										l2 = 3;
+									if (xx == x + (capRad - 1) && zz == z - capRad) {
+										meta = 3;
 									}
 
-									if (i2 == x + l1 && k2 == z - (l1 - 1)) {
-										l2 = 3;
+									if (xx == x + capRad && zz == z - (capRad - 1)) {
+										meta = 3;
 									}
 
-									if (i2 == x - (l1 - 1) && k2 == z + l1) {
-										l2 = 7;
+									if (xx == x - (capRad - 1) && zz == z + capRad) {
+										meta = 7;
 									}
 
-									if (i2 == x - l1 && k2 == z + (l1 - 1)) {
-										l2 = 7;
+									if (xx == x - capRad && zz == z + (capRad - 1)) {
+										meta = 7;
 									}
 
-									if (i2 == x + (l1 - 1) && k2 == z + l1) {
-										l2 = 9;
+									if (xx == x + (capRad - 1) && zz == z + capRad) {
+										meta = 9;
 									}
 
-									if (i2 == x + l1 && k2 == z + (l1 - 1)) {
-										l2 = 9;
+									if (xx == x + capRad && zz == z + (capRad - 1)) {
+										meta = 9;
 									}
 								}
 
-								if (l2 == 5 && k1 < y + i1) {
-									l2 = 0;
+								if (meta == 5 && yy < y + height) {
+									meta = 0;
 								}
 
-								block = world.getBlock(i2, k1, k2);
+								block = world.getBlock(xx, yy, zz);
 
-								if ((l2 != 0 || y >= y + i1 - 1) && (block.getMaterial() == Material.air || block.canBeReplacedByLeaves(world, i2, k1, k2))) {
-									world.setBlock(i2, k1, k2, getMushroomCap(type), l2, 3);
+								if ((meta != 0 || y >= y + height - 1) && (block.getMaterial() == Material.air || block.canBeReplacedByLeaves(world, xx, yy, zz))) {
+									world.setBlock(xx, yy, zz, getMushroomCap(type), meta, 3);
 								}
 							}
 						}
 					}
 
-					for (k1 = 0; k1 < i1; ++k1) {
-						block = world.getBlock(x, y + k1, z);
+					for (yy = 0; yy < height; ++yy) {
+						block = world.getBlock(x, y + yy, z);
 
-						if (block.getMaterial() == Material.air || block.canBeReplacedByLeaves(world, x, y + k1, z)) {
-							world.setBlock(x, y + k1, z, getMushroomCap(type), 10, 3);
+						if (block.getMaterial() == Material.air || block.canBeReplacedByLeaves(world, x, y + yy, z)) {
+							world.setBlock(x, y + yy, z, getMushroomCap(type), 10, 3);
 						}
 					}
 
