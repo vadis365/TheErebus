8c72fcf95cb916ca036aa00061fd37e73a3db314
chylex <chylex@fo2.cz>
2014-07-16 16:25:28 +0200

    /erebus gen

--- src/main/java/erebus/debug/ErebusCommandDebug.java
+++ src/main/java/erebus/debug/ErebusCommandDebug.java
@@ -3,7 +3,9 @@ import net.minecraft.command.CommandBase;
 import net.minecraft.command.ICommandSender;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.ChatComponentText;
+import net.minecraft.util.ChunkCoordinates;
 import net.minecraft.util.IChatComponent;
+import net.minecraft.world.gen.feature.WorldGenerator;
 
 public class ErebusCommandDebug extends CommandBase{
 	private static final IChatComponent text(String str){
@@ -34,7 +36,23 @@ public class ErebusCommandDebug extends CommandBase{
 		
 		if (params.length == 0){
 			sender.addChatMessage(text("Available commands:"));
-			// TODO
+			sender.addChatMessage(text("/erebus gen <type> <feature>"));
 		}
+		else if (params[0].equals("gen") && params.length >= 3){
+			try{
+				Class cls = Class.forName("erebus.world.feature."+params[1]+".WorldGen"+params[2]);
+				WorldGenerator gen = (WorldGenerator)cls.newInstance();
+				ChunkCoordinates coords = sender.getPlayerCoordinates();
+				
+				if (gen.generate(sender.getEntityWorld(),sender.getEntityWorld().rand,coords.posX,coords.posY,coords.posZ)){
+					sender.addChatMessage(text("Generated."));
+				}
+				else sender.addChatMessage(text("Failed."));
+			}catch(Throwable t){
+				t.printStackTrace();
+				sender.addChatMessage(text("Something went wrong."));
+			}
+		}
+		else sender.addChatMessage(text("Wrong command u noob."));
 	}
 }
