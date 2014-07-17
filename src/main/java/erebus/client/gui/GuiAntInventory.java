5f0632bc7f846969ce1a8b203d253e012b416e87
vadis365 <curve_linear@hotmail.com>
2014-07-17 22:22:02 +0100

    an experiment...

--- src/main/java/erebus/client/gui/GuiAntInventory.java
+++ src/main/java/erebus/client/gui/GuiAntInventory.java
@@ -1,9 +1,14 @@
 package erebus.client.gui;
 
 import net.minecraft.client.gui.inventory.GuiContainer;
+import net.minecraft.client.renderer.texture.TextureMap;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.InventoryPlayer;
+import net.minecraft.init.Items;
 import net.minecraft.inventory.IInventory;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.IIcon;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.StatCollector;
 
@@ -18,11 +23,14 @@ import erebus.inventory.ContainerAntInventory;
 public class GuiAntInventory extends GuiContainer {
 
 	private static final ResourceLocation GUI_ANT_INVENTORY = new ResourceLocation("erebus:textures/gui/container/antGuiTest.png");
-
+	public static Item[] ghostIcon = new Item[] {Items.stone_hoe, Items.shears, Items.bucket};
+	private ItemStack stack;
+	int iconCount=0;
 	public GuiAntInventory(InventoryPlayer inventory, Entity entityInventory) {
 		super(new ContainerAntInventory(inventory, (IInventory) entityInventory));
 		xSize = 176;
 		ySize = 131;
+		stack = new ItemStack(ghostIcon[0]);
 	}
 
 	@Override
@@ -39,5 +47,24 @@ public class GuiAntInventory extends GuiContainer {
 		int k = (width - xSize) / 2;
 		int l = (height - ySize) / 2;
 		drawTexturedModalRect(k, l, 0, 0, xSize, ySize);
-	}
+		GL11.glPushMatrix();
+		GL11.glEnable(GL11.GL_BLEND);
+		GL11.glColor4f(1f, 1f, 1f, 0.2f);
+		IIcon iicon1 = stack.getIconIndex();
+		this.mc.renderEngine.bindTexture(TextureMap.locationItemsTexture);
+		this.drawTexturedModelRectFromIcon(guiLeft + 26, guiTop +18, iicon1, 16, 16);
+		GL11.glDisable(GL11.GL_BLEND);
+		GL11.glPopMatrix();
+    }
+		
+	@Override
+    public void updateScreen() {
+        super.updateScreen();
+        if(mc.theWorld.getWorldTime() % 40 == 0) {
+        	stack = new ItemStack(ghostIcon[iconCount]);
+        	iconCount++;
+        	if(iconCount > 2)
+        		iconCount = 0;
+        	}
+        }
 }
