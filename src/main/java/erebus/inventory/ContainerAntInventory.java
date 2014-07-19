0dd15defd125aad11d2fdade43fbc6e280ee97a4
ganymedes01 <foka_12@hotmail.com>
2014-07-19 03:37:52 +0100

    Fix shift-clicking into filter slot

--- src/main/java/erebus/inventory/ContainerAntInventory.java
+++ src/main/java/erebus/inventory/ContainerAntInventory.java
@@ -10,25 +10,20 @@ import erebus.entity.EntityBlackAnt;
 
 public class ContainerAntInventory extends Container {
 
-	private static final int NUM_ROWS = 1;
 	private final IInventory inventory;
 
 	public ContainerAntInventory(InventoryPlayer playerInventory, IInventory entityInventory) {
 		inventory = entityInventory;
 
-		int i = (NUM_ROWS - 4) * 18;
-		int j;
-		int k;
+		int i = 54;
 
-		for (j = 0; j < NUM_ROWS; ++j)
-			for (k = 0; k < 3; ++k)
-				addSlotToContainer(new BetterSlot(entityInventory, k + j * 9, 26 + k * 54, 18 + j * 18));
+		for (int k = 0; k < 3; ++k)
+			addSlotToContainer(new BetterSlot(entityInventory, k, 26 + k * 54, 18));
 
-		for (j = 0; j < 3; ++j)
-			for (k = 0; k < 9; ++k)
+		for (int j = 0; j < 3; ++j)
+			for (int k = 0; k < 9; ++k)
 				addSlotToContainer(new Slot(playerInventory, k + j * 9 + 9, 8 + k * 18, 103 + j * 18 + i));
-
-		for (j = 0; j < 9; ++j)
+		for (int j = 0; j < 9; ++j)
 			addSlotToContainer(new Slot(playerInventory, j, 8 + j * 18, 161 + i));
 	}
 
@@ -46,13 +41,16 @@ public class ContainerAntInventory extends Container {
 			ItemStack is1 = slot.getStack();
 			is = is1.copy();
 
-			if (slotNumber < NUM_ROWS * 9) {
-				if (!mergeItemStack(is1, NUM_ROWS * 9, inventorySlots.size(), true))
+			if (slotNumber == EntityBlackAnt.CROP_ID_SLOT) {
+				slot.putStack(null);
+				return null;
+			} else if (slotNumber < 3) {
+				if (!mergeItemStack(is1, 3, inventorySlots.size(), true))
 					return null;
 			} else if (inventory.isItemValidForSlot(0, is1)) {
 				if (!mergeItemStack(is1, 0, 1, false))
 					return null;
-			} else if (!mergeItemStack(is1, 1, NUM_ROWS * 3, false))
+			} else
 				return null;
 
 			if (is1.stackSize == 0)
