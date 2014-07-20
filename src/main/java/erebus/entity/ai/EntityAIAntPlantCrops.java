9ae65b474ffa5d04701944ca8133ecc7b36ef1d6
vadis365 <curve_linear@hotmail.com>
2014-07-21 00:22:13 +0100

    tidied one of the AI classes a bit

--- src/main/java/erebus/entity/ai/EntityAIAntPlantCrops.java
+++ src/main/java/erebus/entity/ai/EntityAIAntPlantCrops.java
@@ -5,6 +5,7 @@ import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.pathfinding.PathEntity;
 import erebus.core.helper.Utils;
@@ -12,7 +13,11 @@ import erebus.entity.EntityBlackAnt;
 
 public class EntityAIAntPlantCrops extends EntityAIEatBlock {
 
+	EntityBlackAnt blackAnt = (EntityBlackAnt) entity;
+	public static final int CROP_ID_SLOT = 1;
+	public static final int INVENTORY_SLOT = 2;
 	private final double moveSpeed;
+
 	public EntityAIAntPlantCrops(EntityLivingBase entity, double moveSpeed, int eatSpeed) {
 		super((EntityLiving) entity, null, 0, null, moveSpeed, eatSpeed);
 		this.moveSpeed = moveSpeed;
@@ -20,10 +25,9 @@ public class EntityAIAntPlantCrops extends EntityAIEatBlock {
 
 	@Override
 	public boolean shouldExecute() {
-		EntityBlackAnt blackAnt = (EntityBlackAnt) entity;
 		return blackAnt.worldObj.getGameRules().getGameRuleBooleanValue("mobGriefing") && !blackAnt.canCollectFromSilo;
 	}
-	
+
 	@Override
 	protected boolean canEatBlock(Block block, int blockMeta) {
 		if (block == null)
@@ -31,10 +35,10 @@ public class EntityAIAntPlantCrops extends EntityAIEatBlock {
 
 		if (block == Blocks.dirt || block == Blocks.grass)
 			return true;
-		
+
 		else if (block.hasTileEntity(blockMeta))
 			return false;
-		
+
 		return false;
 	}
 
@@ -45,16 +49,17 @@ public class EntityAIAntPlantCrops extends EntityAIEatBlock {
 
 	@Override
 	protected void moveToLocation() {
-		EntityBlackAnt blackAnt = (EntityBlackAnt) entity;
 		PathEntity pathentity = blackAnt.worldObj.getEntityPathToXYZ(blackAnt, cropX, cropY + 1, cropZ, 16.0F, true, false, false, true);
+		
 		if (pathentity != null) {
 			blackAnt.setPathToEntity(pathentity);
 			blackAnt.getNavigator().setPath(pathentity, 0.5D);
 		}
+		
 		if (blackAnt.getDistance(cropX, cropY, cropZ) < 1.5D)
 			blackAnt.getMoveHelper().setMoveTo(cropX + 0.5D, cropY + 1, cropZ + 0.5D, 0.5D);
 	}
-	
+
 	@Override
 	protected void prepareToEat() {
 	}
@@ -64,23 +69,43 @@ public class EntityAIAntPlantCrops extends EntityAIEatBlock {
 	}
 
 	@Override
-	 protected void afterEaten() {
-	  EntityBlackAnt blackAnt = (EntityBlackAnt) entity;
-	if (!blackAnt.worldObj.isRemote) {
-		  if(getTargetBlock() != Blocks.farmland){
-			  Utils.rightClickItemAt(blackAnt.worldObj, cropX, cropY, cropZ, 1, new ItemStack(Items.wooden_hoe));
-			  blackAnt.worldObj.playSoundEffect((double) cropX + 0.5F, (double) cropY + 0.5F, (double) cropZ + 0.5F, Blocks.farmland.stepSound.getStepResourcePath(), (Blocks.farmland.stepSound.getVolume() + 1.0F) / 2.0F, Blocks.farmland.stepSound.getPitch() * 0.8F);
-		  }
-		  if(blackAnt.getStackInSlot(1) != null && blackAnt.getStackInSlot(2) != null) {
-			  if(blackAnt.getStackInSlot(1).getItem() == blackAnt.getStackInSlot(2).getItem()) {
-				  if(blackAnt.getStackInSlot(1).getItemDamage() == blackAnt.getStackInSlot(2).getItemDamage()) {
-						  Utils.rightClickItemAt(blackAnt.worldObj, cropX, cropY, cropZ, 1, new ItemStack(blackAnt.getStackInSlot(2).getItem(), blackAnt.getStackInSlot(2).getItemDamage()));
-						  blackAnt.setInventorySlotContents(2, new ItemStack(blackAnt.getStackInSlot(2).getItem(), blackAnt.getStackInSlot(2).stackSize -1, blackAnt.getStackInSlot(2).getItemDamage()));
-						  if(blackAnt.getStackInSlot(2).stackSize < 1)
-							  blackAnt.setInventorySlotContents(2, null);	 
-				  }
-			  }
-		  }
-	  	}
+	protected void afterEaten() {
+		if (!blackAnt.worldObj.isRemote) {
+			
+			if (getTargetBlock() != Blocks.farmland) {
+				Utils.rightClickItemAt(blackAnt.worldObj, cropX, cropY, cropZ, 1, new ItemStack(Items.wooden_hoe));
+				blackAnt.worldObj.playSoundEffect((double) cropX + 0.5F, (double) cropY + 0.5F, (double) cropZ + 0.5F, Blocks.farmland.stepSound.getStepResourcePath(), (Blocks.farmland.stepSound.getVolume() + 1.0F) / 2.0F, Blocks.farmland.stepSound.getPitch() * 0.8F);
+			}
+			
+			if (!isFilterSlotEmpty() && !isAntInvSlotEmpty()) {
+				Item filterItem = getFilterSlotStack().getItem();
+				Item invItem = getAntInvSlotStack().getItem();
+				int invItemMeta = getAntInvSlotStack().getItemDamage();
+				int filterItemMeta = getFilterSlotStack().getItemDamage();
+
+				if (filterItem == invItem && filterItemMeta == invItemMeta) {
+					Utils.rightClickItemAt(blackAnt.worldObj, cropX, cropY, cropZ, 1, new ItemStack(invItem, invItemMeta));
+					blackAnt.setInventorySlotContents(INVENTORY_SLOT, new ItemStack(invItem, getAntInvSlotStack().stackSize - 1, invItemMeta));
+					if (getAntInvSlotStack().stackSize < 1)
+						blackAnt.setInventorySlotContents(INVENTORY_SLOT, null);
+				}
+			}
+		}
+	}
+
+	public boolean isFilterSlotEmpty() {
+		return getFilterSlotStack() == null;
+	}
+
+	public ItemStack getFilterSlotStack() {
+		return blackAnt.getStackInSlot(CROP_ID_SLOT);
+	}
+
+	public boolean isAntInvSlotEmpty() {
+		return getAntInvSlotStack() == null;
+	}
+
+	public ItemStack getAntInvSlotStack() {
+		return blackAnt.getStackInSlot(INVENTORY_SLOT);
 	}
 }
