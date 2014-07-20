8a88426985903ce764c297c72905f42ee372816d
vadis365 <curve_linear@hotmail.com>
2014-07-21 00:45:45 +0100

    last AI for tonight

--- src/main/java/erebus/entity/ai/EntityAIAntHarvestCrops.java
+++ src/main/java/erebus/entity/ai/EntityAIAntHarvestCrops.java
@@ -9,10 +9,12 @@ import net.minecraft.pathfinding.PathEntity;
 import erebus.entity.EntityBlackAnt;
 
 public class EntityAIAntHarvestCrops extends EntityAIEatBlock {
-
+	
+	EntityBlackAnt blackAnt = (EntityBlackAnt) entity;
 	private final double moveSpeed;
 	private Block blockMunched;
 	private int metaData;
+	
 	public EntityAIAntHarvestCrops(EntityLivingBase entity, double moveSpeed, int eatSpeed) {
 		super((EntityLiving) entity, null, 0, null, moveSpeed, eatSpeed);
 		this.moveSpeed = moveSpeed;
@@ -39,31 +41,29 @@ public class EntityAIAntHarvestCrops extends EntityAIEatBlock {
 
 	@Override
 	protected void moveToLocation() {
-		EntityBlackAnt blackAnt = (EntityBlackAnt) entity;
 		PathEntity pathentity = blackAnt.worldObj.getEntityPathToXYZ(blackAnt, cropX, cropY, cropZ, 16.0F, true, false, false, true);
+		
 		if (pathentity != null) {
 			blackAnt.setPathToEntity(pathentity);
 			blackAnt.getNavigator().setPath(pathentity, 0.5D);
 		}
+		
 		if (blackAnt.getDistance(cropX, cropY, cropZ) < 1.5D)
 			blackAnt.getMoveHelper().setMoveTo(cropX + 0.5D, cropY, cropZ + 0.5D, 0.5D);	
 	}
 
 	@Override
 	protected void prepareToEat() {
-		EntityBlackAnt blackAnt = (EntityBlackAnt) entity;
 		blockMunched = getTargetBlock();
-		metaData = entity.worldObj.getBlockMetadata(cropX, cropY, cropZ);	
+		metaData = blackAnt.worldObj.getBlockMetadata(cropX, cropY, cropZ);	
 	}
 
 	@Override
 	protected void eatingInterupted() {
-		EntityBlackAnt blackAnt = (EntityBlackAnt) entity;
 	}
 
 	@Override
 	protected void afterEaten() {
-		EntityBlackAnt blackAnt = (EntityBlackAnt) entity;
 		blackAnt.worldObj.setBlockToAir(cropX, cropY, cropZ);
 		blackAnt.worldObj.setBlock(cropX, cropY - 1, cropZ, Blocks.dirt);
 		blackAnt.setBlockHarvested(blockMunched, metaData);
