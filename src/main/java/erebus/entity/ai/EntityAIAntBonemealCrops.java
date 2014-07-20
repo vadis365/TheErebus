871c09625577488e0e778e038ca98f34ab4460a7
vadis365 <curve_linear@hotmail.com>
2014-07-21 00:40:06 +0100

    more AI tidying

--- src/main/java/erebus/entity/ai/EntityAIAntBonemealCrops.java
+++ src/main/java/erebus/entity/ai/EntityAIAntBonemealCrops.java
@@ -5,6 +5,7 @@ import net.minecraft.block.BlockCrops;
 import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemDye;
 import net.minecraft.item.ItemStack;
 import net.minecraft.pathfinding.PathEntity;
@@ -13,22 +14,23 @@ import erebus.entity.EntityBlackAnt;
 
 public class EntityAIAntBonemealCrops extends EntityAIEatBlock {
 
+	EntityBlackAnt blackAnt = (EntityBlackAnt) entity;
+	public static final int CROP_ID_SLOT = 1;
+	public static final int INVENTORY_SLOT = 2;
 	private final double moveSpeed;
 
 	public EntityAIAntBonemealCrops(EntityLivingBase entity, double moveSpeed, int eatSpeed) {
 		super((EntityLiving) entity, null, 0, null, moveSpeed, eatSpeed);
 		this.moveSpeed = moveSpeed;
 	}
-	
+
 	@Override
 	public boolean shouldExecute() {
-		EntityBlackAnt blackAnt = (EntityBlackAnt) entity;
 		return blackAnt.worldObj.getGameRules().getGameRuleBooleanValue("mobGriefing") && !blackAnt.canCollectFromSilo;
 	}
-	
+
 	@Override
 	public boolean continueExecuting() {
-		EntityBlackAnt blackAnt = (EntityBlackAnt) entity;
 		return !blackAnt.canCollectFromSilo;
 	}
 
@@ -53,12 +55,13 @@ public class EntityAIAntBonemealCrops extends EntityAIEatBlock {
 
 	@Override
 	protected void moveToLocation() {
-		EntityBlackAnt blackAnt = (EntityBlackAnt) entity;
 		PathEntity pathentity = blackAnt.worldObj.getEntityPathToXYZ(blackAnt, cropX, cropY, cropZ, 16.0F, true, false, false, true);
+		
 		if (pathentity != null) {
 			blackAnt.setPathToEntity(pathentity);
 			blackAnt.getNavigator().setPath(pathentity, 0.5D);
 		}
+		
 		if (blackAnt.getDistance(cropX, cropY, cropZ) < 1.5D)
 			blackAnt.getMoveHelper().setMoveTo(cropX + 0.5D, cropY, cropZ + 0.5D, 0.5D);
 	}
@@ -73,19 +76,39 @@ public class EntityAIAntBonemealCrops extends EntityAIEatBlock {
 
 	@Override
 	protected void afterEaten() {
-		EntityBlackAnt blackAnt = (EntityBlackAnt) entity;
 		EntityPlayer player = Utils.getPlayer(blackAnt.worldObj);
+
 		if (!blackAnt.worldObj.isRemote) {
-			if (blackAnt.getStackInSlot(1) != null && blackAnt.getStackInSlot(2) != null) {
-				if (blackAnt.getStackInSlot(1).getItem() == blackAnt.getStackInSlot(2).getItem()) {
-					if (blackAnt.getStackInSlot(1).getItemDamage() == blackAnt.getStackInSlot(2).getItemDamage()) {
-						Utils.rightClickItemAt(blackAnt.worldObj, cropX, cropY, cropZ, 1, new ItemStack(blackAnt.getStackInSlot(2).getItem(), blackAnt.getStackInSlot(2).getItemDamage()));
-						ItemDye.applyBonemeal(blackAnt.getStackInSlot(2), blackAnt.worldObj, cropX, cropY, cropZ, player);
-					if (blackAnt.getStackInSlot(2).stackSize < 1)
-						blackAnt.setInventorySlotContents(2, null);
-					}
+
+			if (!isFilterSlotEmpty() && !isAntInvSlotEmpty()) {
+				Item filterItem = getFilterSlotStack().getItem();
+				Item invItem = getAntInvSlotStack().getItem();
+				int invItemMeta = getAntInvSlotStack().getItemDamage();
+				int filterItemMeta = getFilterSlotStack().getItemDamage();
+
+				if (filterItem == invItem && filterItemMeta == invItemMeta) {
+					Utils.rightClickItemAt(blackAnt.worldObj, cropX, cropY, cropZ, 1, new ItemStack(invItem, invItemMeta));
+					ItemDye.applyBonemeal(getAntInvSlotStack(), blackAnt.worldObj, cropX, cropY, cropZ, player);
+					if (getAntInvSlotStack().stackSize < 1)
+						blackAnt.setInventorySlotContents(INVENTORY_SLOT, null);
 				}
 			}
 		}
+	}
+
+	public boolean isFilterSlotEmpty() {
+		return getFilterSlotStack() == null;
+	}
+
+	public ItemStack getFilterSlotStack() {
+		return blackAnt.getStackInSlot(CROP_ID_SLOT);
+	}
+
+	public boolean isAntInvSlotEmpty() {
+		return getAntInvSlotStack() == null;
+	}
+
+	public ItemStack getAntInvSlotStack() {
+		return blackAnt.getStackInSlot(INVENTORY_SLOT);
 	}
 }
