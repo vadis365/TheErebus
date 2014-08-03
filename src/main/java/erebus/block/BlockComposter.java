3404bdd6cb5c71bc3ba4b3cb64bf6868722c43f7
vadis365 <curve_linear@hotmail.com>
2014-08-04 00:39:37 +0100

    composter simple block render

--- src/main/java/erebus/block/BlockComposter.java
+++ src/main/java/erebus/block/BlockComposter.java
@@ -6,21 +6,19 @@ import net.minecraft.block.Block;
 import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IIconRegister;
-import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.Container;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.IIcon;
-import net.minecraft.util.MathHelper;
 import net.minecraft.world.World;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import erebus.Erebus;
 import erebus.ModBlocks;
 import erebus.core.helper.Utils;
+import erebus.core.proxy.ClientProxy.BlockRenderIDs;
 import erebus.core.proxy.CommonProxy;
 import erebus.tileentity.TileEntityComposter;
 
@@ -29,7 +27,7 @@ public class BlockComposter extends BlockContainer {
 	private final boolean isActive;
 	private static boolean keepComposterInventory;
 	@SideOnly(Side.CLIENT)
-	private IIcon composterTop, composterFront;
+	private IIcon composterTop, composterBottom;
 
 	public BlockComposter(boolean isActive) {
 		super(Material.rock);
@@ -42,44 +40,17 @@ public class BlockComposter extends BlockContainer {
 	}
 
 	@Override
-	public void onBlockAdded(World world, int x, int y, int z) {
-		super.onBlockAdded(world, x, y, z);
-		setDefaultDirection(world, x, y, z);
-	}
-
-	private void setDefaultDirection(World world, int x, int y, int z) {
-		super.onBlockAdded(world, x, y, z);
-		if (!world.isRemote) {
-			Block block = world.getBlock(x, y, z - 1);
-			Block block1 = world.getBlock(x, y, z + 1);
-			Block block2 = world.getBlock(x - 1, y, z);
-			Block block3 = world.getBlock(x + 1, y, z);
-
-			byte b0 = 3;
-
-			if (block1.func_149730_j() && !block.func_149730_j())
-				b0 = 2;
-			if (block2.func_149730_j() && !block3.func_149730_j())
-				b0 = 5;
-			if (block3.func_149730_j() && !block2.func_149730_j())
-				b0 = 4;
-
-			world.setBlockMetadataWithNotify(x, y, z, b0, 2);
-		}
-	}
-
-	@Override
 	@SideOnly(Side.CLIENT)
 	public IIcon getIcon(int side, int meta) {
-		return side == 1 ? composterTop : side == 0 ? composterTop : side != meta ? blockIcon : composterFront;
+		return side == 1 ? composterTop : side == 0 ? composterBottom : blockIcon;
 	}
 
 	@Override
 	@SideOnly(Side.CLIENT)
 	public void registerBlockIcons(IIconRegister reg) {
-		blockIcon = reg.registerIcon("furnace_side");
-		composterFront = reg.registerIcon("furnace_top");
-		composterTop = reg.registerIcon(isActive ? "furnace_front_on" : "furnace_front_off");
+		blockIcon = reg.registerIcon("erebus:composterSide");
+		composterTop = reg.registerIcon("erebus:composterTop");
+		composterBottom = reg.registerIcon("erebus:composterBottom");
 	}
 
 	@Override
@@ -121,20 +92,6 @@ public class BlockComposter extends BlockContainer {
 	}
 
 	@Override
-	public void onBlockPlacedBy(World world, int x, int y, int z, EntityLivingBase player, ItemStack is) {
-		int l = MathHelper.floor_double(player.rotationYaw * 4.0F / 360.0F + 0.5D) & 3;
-
-		if (l == 0)
-			world.setBlockMetadataWithNotify(x, y, z, 2, 2);
-		if (l == 1)
-			world.setBlockMetadataWithNotify(x, y, z, 5, 2);
-		if (l == 2)
-			world.setBlockMetadataWithNotify(x, y, z, 3, 2);
-		if (l == 3)
-			world.setBlockMetadataWithNotify(x, y, z, 4, 2);
-	}
-
-	@Override
 	public void breakBlock(World world, int x, int y, int z, Block block, int meta) {
 		if (!keepComposterInventory) {
 			Utils.dropInventoryContents(Utils.getTileEntity(world, x, y, z, TileEntityComposter.class));
@@ -148,26 +105,10 @@ public class BlockComposter extends BlockContainer {
 	@SideOnly(Side.CLIENT)
 	public void randomDisplayTick(World world, int x, int y, int z, Random rand) {
 		if (isActive) {
-			int l = world.getBlockMetadata(x, y, z);
 			float f = x + 0.5F;
-			float f1 = y + 0.0F + rand.nextFloat() * 6.0F / 16.0F;
+			float f1 = y + 1.1F + rand.nextFloat() * 6.0F / 16.0F;
 			float f2 = z + 0.5F;
-			float f3 = 0.52F;
-			float f4 = rand.nextFloat() * 0.6F - 0.3F;
-
-			if (l == 4) {
-				world.spawnParticle("smoke", f - f3, f1, f2 + f4, 0.0D, 0.0D, 0.0D);
-				world.spawnParticle("flame", f - f3, f1, f2 + f4, 0.0D, 0.0D, 0.0D);
-			} else if (l == 5) {
-				world.spawnParticle("smoke", f + f3, f1, f2 + f4, 0.0D, 0.0D, 0.0D);
-				world.spawnParticle("flame", f + f3, f1, f2 + f4, 0.0D, 0.0D, 0.0D);
-			} else if (l == 2) {
-				world.spawnParticle("smoke", f + f4, f1, f2 - f3, 0.0D, 0.0D, 0.0D);
-				world.spawnParticle("flame", f + f4, f1, f2 - f3, 0.0D, 0.0D, 0.0D);
-			} else if (l == 3) {
-				world.spawnParticle("smoke", f + f4, f1, f2 + f3, 0.0D, 0.0D, 0.0D);
-				world.spawnParticle("flame", f + f4, f1, f2 + f3, 0.0D, 0.0D, 0.0D);
-			}
+			world.spawnParticle("happyVillager", f, f1, f2, 0.0D, 0.0D, 0.0D);
 		}
 	}
 
@@ -185,5 +126,20 @@ public class BlockComposter extends BlockContainer {
 	@SideOnly(Side.CLIENT)
 	public Item getItem(World world, int x, int y, int z) {
 		return Item.getItemFromBlock(ModBlocks.composter);
+	}
+	
+	@Override
+	public boolean isOpaqueCube() {
+		return false;
+	}
+
+	@Override
+	public boolean renderAsNormalBlock() {
+		return false;
+	}
+	
+	@Override
+	public int getRenderType() {
+		return BlockRenderIDs.COMPOSTER.id();
 	}
 }
