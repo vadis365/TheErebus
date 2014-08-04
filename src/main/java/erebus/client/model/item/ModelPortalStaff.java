1104a7a3f5b2adcf62f1d1965f37c766b4865502
vadis365 <curve_linear@hotmail.com>
2014-08-04 22:00:43 +0100

    yay - something I know how to do!

--- src/main/java/erebus/client/model/item/ModelPortalStaff.java
+++ src/main/java/erebus/client/model/item/ModelPortalStaff.java
@@ -13,29 +13,29 @@ public class ModelPortalStaff extends ModelBase
         textureHeight = 32;
 
         boxes[0] = new ModelRenderer(this, 0, 0);
-        boxes[0].addBox(-2f, -2f, -2f, 4, 4, 4);
-        boxes[0].setRotationPoint(0f, 10f, 0f);
+        boxes[0].addBox(-2F, -2F, -2F, 4, 4, 4);
+        boxes[0].setRotationPoint(0f, 0f, 0f);
         boxes[1] = new ModelRenderer(this, 0, 8);
-        boxes[1].addBox(-.5f, 0f, -0.5f, 1, 14, 1);
-        boxes[1].setRotationPoint(0f, 10f, 0f);
+        boxes[1].addBox(-0.5F, 0F, -0.5F, 1, 14, 1);
+        boxes[1].setRotationPoint(0f, 0f, 0f);
         boxes[2] = new ModelRenderer(this, 16, 4);
-        boxes[2].addBox(-1.5f, 0f, -1.5f, 3, 1, 3);
-        boxes[2].setRotationPoint(0f, 12f, 0f);
+        boxes[2].addBox(-1.5F, 2F, -1.5F, 3, 1, 3);
+        boxes[2].setRotationPoint(0f, 0f, 0f);
         boxes[3] = new ModelRenderer(this, 4, 20);
-        boxes[3].addBox(-1f, 0f, -1f, 2, 1, 2);
-        boxes[3].setRotationPoint(0f, 22f, 0f);
+        boxes[3].addBox(-1F, 12F, -1F, 2, 1, 2);;
+        boxes[3].setRotationPoint(0f, 0f, 0f);
         boxes[4] = new ModelRenderer(this, 5, 9);
-        boxes[4].addBox(-0.5f, 0f, 0.5f, 1, 3, 1);
-        boxes[4].setRotationPoint(0f, 9.5f, -2f);
+        boxes[4].addBox(-0.5F, -0.5F, -2.5F, 1, 3, 1);
+        boxes[4].setRotationPoint(0f, 0f, 0f);
         boxes[5] = new ModelRenderer(this, 5, 9);
-        boxes[5].addBox(-0.5f, 0f, 0.5f, 1, 3, 1);
-        boxes[5].setRotationPoint(0f, 9.5f, 2f);
+        boxes[5].addBox(-0.5F, -0.5F, 1.5F, 1, 3, 1);
+        boxes[5].setRotationPoint(0f, 0f, 0f);
         boxes[6] = new ModelRenderer(this, 5, 9);
-        boxes[6].addBox(-0.5f, 0f, 0.5f, 1, 3, 1);
-        boxes[6].setRotationPoint(-2f, 9.5f, 0f);
+        boxes[6].addBox(-2.5F, -0.5F, -0.5F, 1, 3, 1);
+        boxes[6].setRotationPoint(0f, 0f, 0f);
         boxes[7] = new ModelRenderer(this, 5, 9);
-        boxes[7].addBox(-0.5f, 0f, -0.5f, 1, 3,1);
-        boxes[7].setRotationPoint(2f, 9.5f, 0f);
+        boxes[7].addBox(1.5F, -0.5F, -0.5F, 1, 3, 1);
+        boxes[7].setRotationPoint(0f, 0f, 0f);
     }
 
     public void render()
