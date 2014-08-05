8a271199f1b558e3e3861ea25c7c8bf71ac9f0f2
chylex <chylex@fo2.cz>
2014-08-05 15:36:27 +0200

    Moar shrooms

--- src/main/java/erebus/world/feature/plant/WorldGenGiantMushrooms.java
+++ src/main/java/erebus/world/feature/plant/WorldGenGiantMushrooms.java
@@ -9,10 +9,12 @@ import net.minecraft.util.Direction;
 import net.minecraft.world.World;
 import net.minecraft.world.gen.feature.WorldGenerator;
 import erebus.ModBlocks;
+import erebus.core.helper.MathUtil;
 
 //@formatter:off
 public class WorldGenGiantMushrooms extends WorldGenerator{
-	private static final int stalkMeta = 11;
+	private static final int stalkMeta = 10;
+	private static final int bulbFullMeta = 14;
 	private static final Block tempBlock = Blocks.bedrock;
 	
 	public enum MushroomType{
@@ -41,30 +43,14 @@ public class WorldGenGiantMushrooms extends WorldGenerator{
 		bulbs.clear();
 		Block mushroom = mushroomType.block;
 		
+		// TODO tmp
+		mushroomType = MushroomType.KAIZERS_FINGERS;
+		mushroom = mushroomType.block;
+		//
+		
 		switch(mushroomType){
-			case BULB_CAPPED:
-				int stalkHeight = 3+rand.nextInt(3+rand.nextInt(2));
-				int sideHeight = 1+rand.nextInt(stalkHeight > 3 ? 3 : 2);
-				
-				for(int py = 0; py <= stalkHeight; py++){
-					world.setBlock(x,y++,z,mushroom,stalkMeta,2);
-				}
-				
-				for(int px = -1; px <= 1; px++){
-					for(int pz = -1; pz <= 1; pz++)bulbs.add(new ChunkCoordinates(x+px,y,z+pz));
-				}
-				
-				
-				for(int py = 1; py <= sideHeight; py++){
-					for(int off = -1; off <= 1; off++){
-						bulbs.add(new ChunkCoordinates(x+2,y-py,z+off));
-						bulbs.add(new ChunkCoordinates(x-2,y-py,z+off));
-						bulbs.add(new ChunkCoordinates(x+off,y-py,z+2));
-						bulbs.add(new ChunkCoordinates(x+off,y-py,z-2));
-					}
-				}
-				
-				break;
+			case BULB_CAPPED: genBulbCapped(world,rand,x,y,z,mushroom); break;
+			case KAIZERS_FINGERS: genKaizersFingers(world,rand,x,y,z,mushroom); break;
 		}
 		
 		for(ChunkCoordinates bulb:bulbs)world.setBlock(bulb.posX,bulb.posY,bulb.posZ,tempBlock);
@@ -73,6 +59,94 @@ public class WorldGenGiantMushrooms extends WorldGenerator{
 		
 		return true;
 	}
+	
+	/*
+	 * MUSHROOM TYPE - BULB CAPPED
+	 */
+	
+	private void genBulbCapped(World world, Random rand, int x, int y, int z, Block mushroom){
+		int stalkHeight = 3+rand.nextInt(3+rand.nextInt(2));
+		int sideHeight = 1+rand.nextInt(stalkHeight > 3 ? 3 : 2);
+		
+		for(int py = 0; py <= stalkHeight; py++){
+			world.setBlock(x,y++,z,mushroom,stalkMeta,2);
+		}
+		
+		for(int px = -1; px <= 1; px++){
+			for(int pz = -1; pz <= 1; pz++)bulbs.add(new ChunkCoordinates(x+px,y,z+pz));
+		}
+		
+		
+		for(int py = 1; py <= sideHeight; py++){
+			for(int off = -1; off <= 1; off++){
+				bulbs.add(new ChunkCoordinates(x+2,y-py,z+off));
+				bulbs.add(new ChunkCoordinates(x-2,y-py,z+off));
+				bulbs.add(new ChunkCoordinates(x+off,y-py,z+2));
+				bulbs.add(new ChunkCoordinates(x+off,y-py,z-2));
+			}
+		}
+	}
+	
+	/*
+	 * MUSHROOM TYPE - KAIZERS FINGERS
+	 */
+	
+	private void genKaizersFingers(World world, Random rand, int x, int y, int z, Block mushroom){
+		int mainShroomHeight = 4+rand.nextInt(4);
+		
+		for(int py = 0, sidesPlaced = 0; py <= mainShroomHeight; py++){
+			world.setBlock(x,y+py,z,mushroom,stalkMeta,2);
+			
+			if (py >= 2 && py < mainShroomHeight-1 && rand.nextInt(4+sidesPlaced*2) == 0){
+				int dir = rand.nextInt(4);
+				world.setBlock(x+Direction.offsetX[dir],y+py,z+Direction.offsetZ[dir],mushroom,bulbFullMeta,2);
+				++sidesPlaced;
+			}
+		}
+		
+		bulbs.add(new ChunkCoordinates(x,y+mainShroomHeight+1,z));
+		for(int a = 0; a < 4; a++)bulbs.add(new ChunkCoordinates(x+Direction.offsetX[a],y+mainShroomHeight,z+Direction.offsetZ[a]));
+		
+		List<ChunkCoordinates> connectList = new ArrayList<ChunkCoordinates>();
+		connectList.add(new ChunkCoordinates(x,y-1,z));
+		
+		for(int smallShroomAttempt = 0, xx, zz; smallShroomAttempt < 4+rand.nextInt(7); smallShroomAttempt++){
+			xx = x+rand.nextInt(4)-rand.nextInt(4);
+			zz = z+rand.nextInt(4)-rand.nextInt(4);
+			if (!world.isAirBlock(xx,y,zz) || !world.isAirBlock(xx-1,y,zz) || !world.isAirBlock(xx+1,y,zz) || !world.isAirBlock(xx,y,zz-1) || !world.isAirBlock(xx,y,zz+1))continue;
+			
+			int smallShroomHeight = rand.nextBoolean() ? 1 : 1+rand.nextInt(2);
+			for(int py = 0; py < smallShroomHeight; py++)world.setBlock(xx,y+py,zz,mushroom,stalkMeta,2);
+			world.setBlock(xx,y+smallShroomHeight,zz,mushroom,bulbFullMeta,2);
+			connectList.add(new ChunkCoordinates(xx,y-1,zz));
+		}
+		
+		int coordAmt = connectList.size();
+		
+		for(int connectionAttempt = 0, dir, xx, zz; connectionAttempt < 48; connectionAttempt++){
+			ChunkCoordinates coords1 = connectList.get(rand.nextInt(coordAmt));
+			ChunkCoordinates coords2 = rand.nextInt(3) != 0 ? connectList.get(0) : connectList.get(rand.nextInt(coordAmt));
+			if (coords1 == coords2)continue;
+			
+			double dist = MathUtil.distance(coords1.posX-coords2.posX,coords1.posZ-coords2.posZ);
+			if (dist < 1D)continue;
+			
+			dir = rand.nextInt(4);
+			xx = coords1.posX+Direction.offsetX[dir];
+			zz = coords1.posZ+Direction.offsetZ[dir];
+			
+			if (MathUtil.distance(xx-coords2.posX,zz-coords2.posZ) < dist){
+				world.setBlock(xx,y-1,zz,mushroom,stalkMeta,2);
+				if (rand.nextInt(16) == 0)world.setBlock(xx,y,zz,mushroom,stalkMeta,2);
+				coords1.posX = xx;
+				coords1.posZ = zz;
+			}
+		}
+	}
+	
+	/*
+	 * METADATA UTILITY
+	 */
 	
 	public static int getMetadata(World world, int centerX, int centerZ, ChunkCoordinates bulb){
 		boolean posX = world.getBlock(bulb.posX+1,bulb.posY,bulb.posZ) == tempBlock,
