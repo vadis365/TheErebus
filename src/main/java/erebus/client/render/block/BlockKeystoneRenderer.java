af1035db07298a948f53002d37518cf604a08aaf
vadis365 <curve_linear@hotmail.com>
2014-08-05 21:08:25 +0100

    eye block thingy render

--- src/main/java/erebus/client/render/block/BlockKeystoneRenderer.java
+++ src/main/java/erebus/client/render/block/BlockKeystoneRenderer.java
@@ -1,48 +1,52 @@
 package erebus.client.render.block;
 
-import cpw.mods.fml.client.registry.ISimpleBlockRenderingHandler;
-import erebus.block.BlockGaeanKeystone;
-import erebus.core.proxy.ClientProxy;
 import net.minecraft.block.Block;
 import net.minecraft.client.renderer.RenderBlocks;
 import net.minecraft.world.IBlockAccess;
+import cpw.mods.fml.client.registry.ISimpleBlockRenderingHandler;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import erebus.ModBlocks;
+import erebus.block.BlockGaeanKeystone;
+import erebus.core.proxy.ClientProxy.BlockRenderIDs;
+@SideOnly(Side.CLIENT)
 
-public class BlockKeystoneRenderer implements ISimpleBlockRenderingHandler
-{
-    public void renderInventoryBlock(Block block, int metadata, int modelId, RenderBlocks renderer)
-    {
+public class BlockKeystoneRenderer implements ISimpleBlockRenderingHandler {
 
+	@Override
+    public void renderInventoryBlock(Block block, int metadata, int modelId, RenderBlocks renderer) {
+        renderer.renderAllFaces = true;
+        renderer.setRenderBounds(0.0D, 0.0D, 0.0D, 1.0D, 0.8125D, 1.0D);
+        BlockRenderHelper.renderSimpleBlock(ModBlocks.gaeanKeystone, 0, renderer);
+        renderer.renderAllFaces = false;
     }
 
-    public boolean renderWorldBlock(IBlockAccess world, int x, int y, int z, Block block, int modelId, RenderBlocks renderer)
-    {
+	@Override
+    public boolean renderWorldBlock(IBlockAccess world, int x, int y, int z, Block block, int modelId, RenderBlocks renderer) {
         renderKeystone(renderer, (BlockGaeanKeystone) block, x, y, z);
         return true;
     }
 
-    public boolean shouldRender3DInInventory(int modelId)
-    {
-        return false;
+	@Override
+    public boolean shouldRender3DInInventory(int modelId) {
+        return true;
     }
 
-    public int getRenderId()
-    {
-        return ClientProxy.BlockRenderIDs.KEYSTONE.id();
+	@Override
+    public int getRenderId() {
+        return BlockRenderIDs.KEYSTONE.id();
     }
 
-    public boolean renderKeystone(RenderBlocks renderer, BlockGaeanKeystone block, int x, int y, int z)
-    {
+    public boolean renderKeystone(RenderBlocks renderer, BlockGaeanKeystone block, int x, int y, int z) {
         int l = renderer.blockAccess.getBlockMetadata(x, y, z);
 
-        if (!BlockGaeanKeystone.isGemActive(l))
-        {
+        if (!BlockGaeanKeystone.isGemActive(l)) {
             renderer.setRenderBounds(0.0D, 0.0D, 0.0D, 1.0D, 0.8125D, 1.0D);
             renderer.renderStandardBlock(block, x, y, z);
             renderer.uvRotateTop = 0;
             return true;
         }
-        else
-        {
+        else {
             renderer.renderAllFaces = true;
             renderer.setRenderBounds(0.0D, 0.0D, 0.0D, 1.0D, 0.8125D, 1.0D);
             renderer.renderStandardBlock(block, x, y, z);
