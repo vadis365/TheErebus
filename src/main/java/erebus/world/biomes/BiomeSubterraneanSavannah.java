648c5c0b2228bfd2b811917402d8cc4d3942e303
ganymedes01 <foka_12@hotmail.com>
2014-08-07 16:16:58 +0100

    Closes #155

--- src/main/java/erebus/world/biomes/BiomeSubterraneanSavannah.java
+++ src/main/java/erebus/world/biomes/BiomeSubterraneanSavannah.java
@@ -7,7 +7,6 @@ import erebus.entity.EntityChameleonTick;
 import erebus.entity.EntityFly;
 import erebus.entity.EntityGlowWorm;
 import erebus.entity.EntityGrasshopper;
-import erebus.entity.EntityJumpingSpider;
 import erebus.entity.EntityMosquito;
 import erebus.entity.EntityScorpion;
 import erebus.entity.EntityScytodes;
@@ -20,13 +19,13 @@ public class BiomeSubterraneanSavannah extends BiomeBaseErebus{
 	@SuppressWarnings("unchecked")
 	public BiomeSubterraneanSavannah(int biomeID){
 		super(biomeID,new BiomeDecoratorSubterraneanSavannah());
-		
+
 		setBiomeName("Subterranean Savannah");
 		setColors(0xB6B957,0xA3A52D);
 		setFog(140,116,9);
 		setTemperatureRainfall(0.95F,0.05F);
 		setWeight(18);
-		
+
 		spawnableCreatureList.add(new SpawnEntry(EntityGrasshopper.class,14,4,8));
 		spawnableCreatureList.add(new SpawnEntry(EntityGlowWorm.class,14,4,8));
 
@@ -34,7 +33,6 @@ public class BiomeSubterraneanSavannah extends BiomeBaseErebus{
 		spawnableMonsterList.add(new SpawnEntry(EntityWasp.class,30,4,8));
 		spawnableMonsterList.add(new SpawnEntry(EntityTarantula.class,18,4,8));
 		spawnableMonsterList.add(new SpawnEntry(EntityScorpion.class,10,4,8));
-		spawnableMonsterList.add(new SpawnEntry(EntityJumpingSpider.class,10,1,4));
 		spawnableMonsterList.add(new SpawnEntry(EntityChameleonTick.class,10,1,2));
 		spawnableCaveCreatureList.add(new SpawnEntry(EntityMosquito.class,60,1,3));
 
