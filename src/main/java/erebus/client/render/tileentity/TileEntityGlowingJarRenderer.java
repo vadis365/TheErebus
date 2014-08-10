b5573bc257c2aa677152b2a50b8eb1786892da25
ganymedes01 <foka_12@hotmail.com>
2014-08-10 15:01:33 +0100

    Avoid creating an EntityItem every render tick

--- src/main/java/erebus/client/render/tileentity/TileEntityGlowingJarRenderer.java
+++ src/main/java/erebus/client/render/tileentity/TileEntityGlowingJarRenderer.java
@@ -6,9 +6,9 @@ import net.minecraft.client.renderer.RenderBlocks;
 import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.entity.RenderItem;
 import net.minecraft.client.renderer.entity.RenderManager;
+import net.minecraft.client.renderer.texture.TextureMap;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
 import net.minecraft.entity.item.EntityItem;
-import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ResourceLocation;
 
@@ -17,8 +17,8 @@ import org.lwjgl.opengl.GL11;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import erebus.ModBlocks;
-import erebus.ModItems;
 import erebus.client.model.block.ModelGlowingJar;
+import erebus.tileentity.TileEntityGlowingJar;
 import erebus.tileentity.TileEntityJarOHoney;
 
 @SideOnly(Side.CLIENT)
@@ -26,12 +26,8 @@ public class TileEntityGlowingJarRenderer extends TileEntitySpecialRenderer {
 
 	private final ModelGlowingJar glowingJar = new ModelGlowingJar();
 	private final RenderItem renderItem;
-	private final ItemStack glowThingy = new ItemStack(ModItems.erebusMaterials, 1, 13);
 	private final RenderBlocks blockRenderer = new RenderBlocks();
-    private final ResourceLocation textures = new ResourceLocation("erebus:textures/blocks/blockAmber.png");
-    private final ResourceLocation textures2 = new ResourceLocation("erebus:textures/special/tiles/glowingJar.png");
-    private final FontRenderer fontrenderer = Minecraft.getMinecraft().fontRenderer;;
-    private EntityItem ghostEntityItem;
+    private static final ResourceLocation GLOWING_JAR = new ResourceLocation("erebus:textures/special/tiles/glowingJar.png");
 
 	public TileEntityGlowingJarRenderer() {
 		renderItem = new RenderItem() {
@@ -45,7 +41,6 @@ public class TileEntityGlowingJarRenderer extends TileEntitySpecialRenderer {
 
 	@Override
 	public void renderTileEntityAt(TileEntity tile, double x, double y, double z, float partialTickTime) {
-        if (ghostEntityItem == null) ghostEntityItem = new EntityItem(tile.getWorldObj());
 		if (tile instanceof TileEntityJarOHoney) {
 			int amount = ((TileEntityJarOHoney) tile).tank.getFluidAmount();
 			int capacity = ((TileEntityJarOHoney) tile).tank.getCapacity();
@@ -56,24 +51,23 @@ public class TileEntityGlowingJarRenderer extends TileEntitySpecialRenderer {
 				GL11.glBlendFunc(770, 771);
 				GL11.glTranslated((float) x + 0.5F, (float) (y + 0.030F + size * 0.5F), (float) z + 0.5F);
 				GL11.glScalef(0.55F, -size, -0.55F);
-				bindTexture(textures);
+				bindTexture(TextureMap.locationBlocksTexture);
 				blockRenderer.renderBlockAsItem(ModBlocks.blockAmber, 0, 1.0F);
 				GL11.glDisable(3042);
 				GL11.glPopMatrix();
 			}
 			renderNameTag(((TileEntityJarOHoney) tile).getOwnerName(), x, y, z);
-		} else {
-			ghostEntityItem.hoverStart = 0.0F;
-			ghostEntityItem.setEntityItemStack(glowThingy);
+		} else if (tile instanceof TileEntityGlowingJar) {
+			EntityItem ghostItem = ((TileEntityGlowingJar)tile).getGhostItem();
 			GL11.glPushMatrix();
 			GL11.glTranslatef((float) x + 0.5F, (float) (y + 0.2F), (float) z + 0.5F);
 			GL11.glScalef(1.2F, 1.2F, 1.2F);
-			renderItem.doRender(ghostEntityItem, 0, 0, 0, 0, 0);
+			renderItem.doRender(ghostItem, 0, 0, 0, 0, 0);
 			GL11.glRotatef(90, 0, 1, 0);
-			renderItem.doRender(ghostEntityItem, 0, 0, 0, 0, 0);
+			renderItem.doRender(ghostItem, 0, 0, 0, 0, 0);
 			GL11.glPopMatrix();
 		}
-		bindTexture(textures2);
+		bindTexture(GLOWING_JAR);
 		GL11.glPushMatrix();
 		GL11.glTranslatef((float) x + 0.5F, (float) y + 1.51F, (float) z + 0.5F);
 		GL11.glScalef(0.7F, -1F, -0.7F);
@@ -102,6 +96,7 @@ public class TileEntityGlowingJarRenderer extends TileEntitySpecialRenderer {
 		GL11.glDisable(GL11.GL_TEXTURE_2D);
 
 		tessellator.startDrawingQuads();
+		FontRenderer fontrenderer = Minecraft.getMinecraft().fontRenderer;
 		int width = fontrenderer.getStringWidth(name) / 2;
 		tessellator.setColorRGBA_F(0.0F, 0.0F, 0.0F, 0.25F);
 		tessellator.addVertex(-width - 1, -1, 0.0D);
--- src/main/java/erebus/tileentity/TileEntityGlowingJar.java
+++ src/main/java/erebus/tileentity/TileEntityGlowingJar.java
@@ -1,9 +1,27 @@
 package erebus.tileentity;
 
+import net.minecraft.entity.item.EntityItem;
+import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import erebus.ModItems;
 
 public class TileEntityGlowingJar extends TileEntity {
 
+	@SideOnly(Side.CLIENT)
+	private EntityItem ghostItem;
+	
+	@SideOnly(Side.CLIENT)
+	public EntityItem getGhostItem() {
+		if(ghostItem == null) {
+			ghostItem = new EntityItem(worldObj);
+			ghostItem.hoverStart = 0.0F;
+			ghostItem.setEntityItemStack(new ItemStack(ModItems.erebusMaterials, 1, 13));
+		}
+		return ghostItem;
+	}
+	
 	@Override
 	public boolean canUpdate() {
 		return false;
