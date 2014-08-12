e6a22ed40753d6036fa4c9026792ed01f124f448
chylex <chylex@fo2.cz>
2014-08-12 12:35:39 +0200

    Libs no work, adding an exception to my system
    If you don't like this, help me fix the libs. Until then, this is going
    to stay.

--- src/main/java/erebus/integration/ModIntegrationHandler.java
+++ src/main/java/erebus/integration/ModIntegrationHandler.java
@@ -2,13 +2,18 @@ package erebus.integration;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import cpw.mods.fml.common.Loader;
 
 public class ModIntegrationHandler {
 	private static List<IModIntegration> integratedMods = new ArrayList<IModIntegration>();
 
 	public static void addMod(Class<? extends IModIntegration> cls) {
+		try{
+			String runFile = ModIntegrationHandler.class.getProtectionDomain().getCodeSource().getLocation().getFile();
+			if (runFile.equals("/D:/MCP/1.7.10-EREBUS/bin/erebus/integration/ModIntegrationHandler.class"))return;
+			// fuck the integration, I'm disabling all integration loading in my workspace
+		}catch(Throwable t){}
+		
 		try {
 			IModIntegration obj = cls.newInstance();
 			if (Loader.isModLoaded(obj.getModId()))
