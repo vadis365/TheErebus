f009d72f9842ac1ae8105a5bc1794160a2d491cf
ganymedes01 <foka_12@hotmail.com>
2014-08-28 18:27:00 +0100

    A bit more efficient than lexiconn's way of checking if the portal is formed =P

--- src/main/java/erebus/tileentity/TileEntityPortal.java
+++ src/main/java/erebus/tileentity/TileEntityPortal.java
@@ -1,9 +1,7 @@
 package erebus.tileentity;
 
-import net.minecraft.block.Block;
 import net.minecraft.tileentity.TileEntity;
 import erebus.ModBlocks;
-import erebus.block.BlockErebusPortal;
 
 public class TileEntityPortal extends TileEntity
 {
@@ -15,27 +13,18 @@ public class TileEntityPortal extends TileEntity
 	@Override
 	public void updateEntity()
 	{
-		BlockErebusPortal p;
-		Block b = worldObj.getBlock(xCoord, yCoord, zCoord);
-		Block topBlock = worldObj.getBlock(xCoord, yCoord + 1, zCoord);
-		if (topBlock instanceof BlockErebusPortal)
+		if (worldObj.isRemote)
 		{
-			if (b instanceof BlockErebusPortal)
+			return;
+		}
+
+		if (worldObj.getTotalWorldTime() % 20L == 0L)
+		{
+			if (!ModBlocks.portal.isPatternValid(worldObj, xCoord, yCoord, zCoord))
 			{
-				p = (BlockErebusPortal) b;
-			} else
-			{
-				return;
-			}
-			if (!p.isPatternValid(worldObj, xCoord, yCoord, zCoord))
-			{
-				for (int l = 0; l < 80; ++l)
-				{
-					worldObj.spawnParticle("hugeexplosion", xCoord + Math.random(), yCoord + Math.random(), zCoord + Math.random(), 0.0D, 0.0D, 0.0D);
-				}
 				worldObj.setBlockToAir(xCoord, yCoord, zCoord);
 				worldObj.setBlockToAir(xCoord, yCoord + 1, zCoord);
-                worldObj.setBlockMetadataWithNotify(xCoord, yCoord + 2, zCoord, 0, 2);
+				worldObj.setBlockMetadataWithNotify(xCoord, yCoord + 2, zCoord, 0, 2);
 			}
 		}
 	}
