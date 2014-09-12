e46e7500d83e677eebdde6e5ba16206c5d1eca30
ganymedes01 <foka_12@hotmail.com>
2014-09-12 17:16:36 +0100

    Cleaned up teleporter. Now it properly finds a already created portal

--- src/main/java/erebus/world/teleporter/TeleporterErebus.java
+++ src/main/java/erebus/world/teleporter/TeleporterErebus.java
@@ -6,7 +6,6 @@ import java.util.List;
 
 import net.minecraft.entity.Entity;
 import net.minecraft.init.Blocks;
-import net.minecraft.util.Direction;
 import net.minecraft.util.LongHashMap;
 import net.minecraft.util.MathHelper;
 import net.minecraft.world.ChunkCoordIntPair;
@@ -39,55 +38,46 @@ final class TeleporterErebus extends Teleporter
 	@Override
 	public boolean placeInExistingPortal(Entity entity, double x, double y, double z, float par8)
 	{
-		short checkRad = 32;
-		double d3 = -1.0D;
-		int i = 0;
-		int j = 0;
-		int k = 0;
-		int int_x = MathHelper.floor_double(entity.posX);
-		int int_z = MathHelper.floor_double(entity.posZ);
-		long j1 = ChunkCoordIntPair.chunkXZ2Int(int_x, int_z);
-		boolean flag = true;
-		double d4;
-		int k1;
+		int checkRadius = 32;
+		double distToPortal = -1.0;
+		int posX = 0;
+		int posY = 0;
+		int posZ = 0;
+		int roundX = MathHelper.floor_double(entity.posX);
+		int roundZ = MathHelper.floor_double(entity.posZ);
+		long coordPair = ChunkCoordIntPair.chunkXZ2Int(roundX, roundZ);
+		boolean portalNotSaved = true;
 
-		if (destinationCoordinateCache.containsItem(j1))
+		if (destinationCoordinateCache.containsItem(coordPair))
 		{
-			PortalPosition portalposition = (PortalPosition) destinationCoordinateCache.getValueByKey(j1);
-			d3 = 0.0D;
-			i = portalposition.posX;
-			j = portalposition.posY;
-			k = portalposition.posZ;
-			portalposition.lastUpdateTime = worldServerInstance.getTotalWorldTime();
-			flag = false;
+			PortalPosition pos = (PortalPosition) destinationCoordinateCache.getValueByKey(coordPair);
+			distToPortal = 0.0;
+			posX = pos.posX;
+			posY = pos.posY;
+			posZ = pos.posZ;
+			pos.lastUpdateTime = worldServerInstance.getTotalWorldTime();
+			portalNotSaved = false;
 		} else
 		{
-			for (k1 = int_x - checkRad; k1 <= int_x + checkRad; ++k1)
+			for (int i = roundX - checkRadius; i <= roundX + checkRadius; i++)
 			{
-				double d5 = k1 + 0.5D - entity.posX;
-
-				for (int l1 = int_z - checkRad; l1 <= int_z + checkRad; ++l1)
+				for (int j = roundZ - checkRadius; j <= roundZ + checkRadius; j++)
 				{
-					double d6 = l1 + 0.5D - entity.posZ;
-
-					for (int i2 = worldServerInstance.getActualHeight() - 1; i2 >= 0; --i2)
+					for (int h = worldServerInstance.getActualHeight() - 1; h >= 0; h--)
 					{
-						if (worldServerInstance.getBlock(k1, i2, l1) == ModBlocks.portal)
+						if (worldServerInstance.getBlock(i, h, j) == ModBlocks.gaeanKeystone)
 						{
-							while (worldServerInstance.getBlock(k1, i2 - 1, l1) == ModBlocks.portal)
-							{
-								--i2;
-							}
+							double X = i + 0.5 - entity.posX;
+							double Y = j + 0.5 - entity.posZ;
+							double Z = h - 2 + 0.5 - entity.posY;
+							double dist = X * X + Z * Z + Y * Y;
 
-							d4 = i2 + 0.5D - entity.posY;
-							double d7 = d5 * d5 + d4 * d4 + d6 * d6;
-
-							if (d3 < 0.0D || d7 < d3)
+							if (distToPortal < 0.0 || dist < distToPortal)
 							{
-								d3 = d7;
-								i = k1;
-								j = i2;
-								k = l1;
+								distToPortal = dist;
+								posX = i;
+								posY = h;
+								posZ = j;
 							}
 						}
 					}
@@ -95,122 +85,20 @@ final class TeleporterErebus extends Teleporter
 			}
 		}
 
-		if (d3 >= 0.0D)
+		if (distToPortal >= 0.0)
 		{
-			if (flag)
+			if (portalNotSaved)
 			{
-				destinationCoordinateCache.add(j1, new PortalPosition(i, j, k, worldServerInstance.getTotalWorldTime()));
-				destinationCoordinateKeys.add(Long.valueOf(j1));
+				destinationCoordinateCache.add(coordPair, new PortalPosition(posX, posY, posZ, worldServerInstance.getTotalWorldTime()));
+				destinationCoordinateKeys.add(Long.valueOf(coordPair));
 			}
 
-			double d8 = i + 0.5D;
-			double d9 = j + 0.5D;
-			d4 = k + 0.5D;
-			int j2 = -1;
-
-			if (worldServerInstance.getBlock(i - 1, j, k) == ModBlocks.portal)
-			{
-				j2 = 2;
-			}
-
-			if (worldServerInstance.getBlock(i + 1, j, k) == ModBlocks.portal)
-			{
-				j2 = 0;
-			}
-
-			if (worldServerInstance.getBlock(i, j, k - 1) == ModBlocks.portal)
-			{
-				j2 = 3;
-			}
-
-			if (worldServerInstance.getBlock(i, j, k + 1) == ModBlocks.portal)
-			{
-				j2 = 1;
-			}
-
-			int k2 = entity.getTeleportDirection();
-
-			if (j2 > -1)
-			{
-				int l2 = Direction.rotateLeft[j2];
-				int i3 = Direction.offsetX[j2];
-				int j3 = Direction.offsetZ[j2];
-				int k3 = Direction.offsetX[l2];
-				int l3 = Direction.offsetZ[l2];
-				boolean flag1 = !worldServerInstance.isAirBlock(i + i3 + k3, j, k + j3 + l3) || !worldServerInstance.isAirBlock(i + i3 + k3, j + 1, k + j3 + l3);
-				boolean flag2 = !worldServerInstance.isAirBlock(i + i3, j, k + j3) || !worldServerInstance.isAirBlock(i + i3, j + 1, k + j3);
-
-				if (flag1 && flag2)
-				{
-					j2 = Direction.rotateOpposite[j2];
-					l2 = Direction.rotateOpposite[l2];
-					i3 = Direction.offsetX[j2];
-					j3 = Direction.offsetZ[j2];
-					k3 = Direction.offsetX[l2];
-					l3 = Direction.offsetZ[l2];
-					k1 = i - k3;
-					d8 -= k3;
-					int i4 = k - l3;
-					d4 -= l3;
-					flag1 = !worldServerInstance.isAirBlock(k1 + i3 + k3, j, i4 + j3 + l3) || !worldServerInstance.isAirBlock(k1 + i3 + k3, j + 1, i4 + j3 + l3);
-					flag2 = !worldServerInstance.isAirBlock(k1 + i3, j, i4 + j3) || !worldServerInstance.isAirBlock(k1 + i3, j + 1, i4 + j3);
-				}
-
-				float f1 = 0.5F;
-				float f2 = 0.5F;
-
-				if (!flag1 && flag2)
-				{
-					f1 = 1.0F;
-				} else if (flag1 && !flag2)
-				{
-					f1 = 0.0F;
-				} else if (flag1 && flag2)
-				{
-					f2 = 0.0F;
-				}
-
-				d8 += k3 * f1 + f2 * i3;
-				d4 += l3 * f1 + f2 * j3;
-				float f3 = 0.0F;
-				float f4 = 0.0F;
-				float f5 = 0.0F;
-				float f6 = 0.0F;
-
-				if (j2 == k2)
-				{
-					f3 = 1.0F;
-					f4 = 1.0F;
-				} else if (j2 == Direction.rotateOpposite[k2])
-				{
-					f3 = -1.0F;
-					f4 = -1.0F;
-				} else if (j2 == Direction.rotateRight[k2])
-				{
-					f5 = 1.0F;
-					f6 = -1.0F;
-				} else
-				{
-					f5 = -1.0F;
-					f6 = 1.0F;
-				}
-
-				double d10 = entity.motionX;
-				double d11 = entity.motionZ;
-				entity.motionX = d10 * f3 + d11 * f6;
-				entity.motionZ = d10 * f5 + d11 * f4;
-				entity.rotationYaw = par8 - k2 * 90 + j2 * 90;
-			} else
-			{
-				entity.motionX = entity.motionY = entity.motionZ = 0.0D;
-			}
-
-			entity.setLocationAndAngles(d8, d9, d4, entity.rotationYaw, entity.rotationPitch);
+			entity.motionX = entity.motionY = entity.motionZ = 0.0;
+			entity.setLocationAndAngles(posX + 0.5, posY + 0.5, posZ + 0.5, entity.rotationYaw, entity.rotationPitch);
 			return true;
-		} else
-		{
-			return false;
 		}
+
+		return false;
 	}
 
 	@Override
@@ -276,22 +164,20 @@ final class TeleporterErebus extends Teleporter
 	}
 
 	@Override
-	public void removeStalePortalLocations(long par1)
+	public void removeStalePortalLocations(long timer)
 	{
-		if (par1 % 100L == 0L)
+		if (timer % 100L == 0L)
 		{
 			Iterator<Long> iterator = destinationCoordinateKeys.iterator();
-			long j = par1 - 600L;
-
 			while (iterator.hasNext())
 			{
-				Long olong = iterator.next();
-				PortalPosition portalposition = (PortalPosition) destinationCoordinateCache.getValueByKey(olong.longValue());
+				Long hashedPortalPos = iterator.next();
+				PortalPosition position = (PortalPosition) destinationCoordinateCache.getValueByKey(hashedPortalPos.longValue());
 
-				if (portalposition == null || portalposition.lastUpdateTime < j)
+				if (position == null || position.lastUpdateTime < timer - 600L)
 				{
 					iterator.remove();
-					destinationCoordinateCache.remove(olong.longValue());
+					destinationCoordinateCache.remove(hashedPortalPos.longValue());
 				}
 			}
 		}
