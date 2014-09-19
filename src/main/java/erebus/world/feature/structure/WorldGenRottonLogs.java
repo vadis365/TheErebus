bdaca853e730c5e369920f5e151043f4940674a6
iLexiconn <ilexiconn@gmail.com>
2014-09-19 17:30:21 +0200

    Can someone register this thing?

---
+++ src/main/java/erebus/world/feature/structure/WorldGenRottonLogs.java
@@ -0,0 +1,16 @@
+package erebus.world.feature.structure;
+
+import net.minecraft.init.Blocks;
+import net.minecraft.world.World;
+import net.minecraft.world.gen.feature.WorldGenerator;
+
+import java.util.Random;
+
+public class WorldGenRottonLogs extends WorldGenerator
+{
+    public boolean generate(World world, Random random, int x, int y, int z)
+    {
+        world.setBlock(x, y, z, Blocks.dirt);
+        return false;
+    }
+}
