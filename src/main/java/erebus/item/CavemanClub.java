88caff825b9006ae473f359f8e0e1aef3dd3e2a2
vadis365 <curve_linear@hotmail.com>
2014-09-21 01:42:51 +0100

    Big stump mini dungeon

--- src/main/java/erebus/item/CavemanClub.java
+++ src/main/java/erebus/item/CavemanClub.java
@@ -29,7 +29,9 @@ public class CavemanClub extends ItemSword
 	public boolean onItemUse(ItemStack is, EntityPlayer player, World world, int x, int y, int z, int side, float hitX, float hitY, float hitZ) {
 		if (!world.isRemote && player.isSneaking()) {
 			Random rand = new Random();
-			new WorldGenRottenLogs(rand.nextInt(11)+5, rand.nextInt(3)+3).generate(world, rand, x, y+1, z);
+			//new WorldGenRottenLogs(rand.nextInt(11)+5, rand.nextInt(4)+3).generate(world, rand, x, y+1, z);
+			new WorldGenRottenLogs(15, 6).generate(world, rand, x, y+1, z);
+			
 			return true;
 		}
 		return false;
