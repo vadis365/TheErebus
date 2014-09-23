b42115ddcdbae131bb76cfdeb2bda5e265e28e02
vadis365 <curve_linear@hotmail.com>
2014-09-23 02:06:33 +0100

    fallen logs wip

--- src/main/java/erebus/item/CavemanClub.java
+++ src/main/java/erebus/item/CavemanClub.java
@@ -18,5 +18,17 @@ public class CavemanClub extends ItemSword
 	{
 		return Items.bone == itemStack2.getItem() ? true : super.getIsRepairable(itemStack1, itemStack2);
 	}
-
+	
+	/*//Just in case
+	// Remove onItemUse method completely after testing hollow logs is over!!!!
+	@Override
+	public boolean onItemUse(ItemStack is, EntityPlayer player, World world, int x, int y, int z, int side, float hitX, float hitY, float hitZ) {
+		Random rand = new Random();
+		if (!world.isRemote && player.isSneaking()) {
+			new WorldGenRottenLogs(rand.nextInt(8)+4, rand.nextInt(3)+2, (byte)rand.nextInt(2)).generate(world, rand, x, y+1, z);
+			return true;
+		}
+		return false;
+	}
+	*/
 }
