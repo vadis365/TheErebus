62e1d23ef8b4b4658a6137f46af47b749b70f717
vadis365 <curve_linear@hotmail.com>
2014-10-03 02:58:17 +0100

    Place holder rubbish code
    I have to change this to AABB and add drops to the block. too late need
    zzzzzzzz.

--- src/main/java/erebus/item/SpiderTShirt.java
+++ src/main/java/erebus/item/SpiderTShirt.java
@@ -1,9 +1,12 @@
 package erebus.item;
 
+import net.minecraft.block.Block;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemArmor;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.MathHelper;
 import net.minecraft.world.World;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
@@ -33,6 +36,18 @@ public class SpiderTShirt extends ItemArmor
 	@Override
 	public void onArmorTick(World world, EntityPlayer player, ItemStack itemStack)
 	{
-		//negate web
+		int x = MathHelper.floor_double(player.posX);
+		int y = MathHelper.floor_double(player.posY + 0.5D);
+		int z = MathHelper.floor_double(player.posZ);
+		
+		Block blockBelow = world.getBlock(x, y - 1, z);
+		Block blockBottom = world.getBlock(x, y, z);
+		Block blockTop = world.getBlock(x, y + 1, z);
+		if (blockBelow != null && blockBelow == Blocks.web)
+			world.setBlock(x, y - 1, z, Blocks.air);
+		if (blockBottom != null && blockBottom == Blocks.web)
+			world.setBlock(x, y, z, Blocks.air);
+		if (blockTop != null && blockTop == Blocks.web)
+			world.setBlock(x, y + 1, z, Blocks.air);
 	}
 }
