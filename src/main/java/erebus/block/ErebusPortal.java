9320a2126bd5e967e5c7e53e073ab8062856dcc1
vadis365 <curve_linear@hotmail.com>
2014-10-15 21:30:56 +0100

    whoops

--- src/main/java/erebus/block/ErebusPortal.java
+++ src/main/java/erebus/block/ErebusPortal.java
@@ -152,7 +152,7 @@ public class ErebusPortal extends Block
 			}
 			if (entity != null)
 			{
-				entity.timeUntilPortal = ConfigHandler.INSTANCE.portalCooldown * 20; //or set this to an arbitrary amount
+				entity.timeUntilPortal = ConfigHandler.INSTANCE.portalCooldown * 20;
 			}
 		}
 	}
