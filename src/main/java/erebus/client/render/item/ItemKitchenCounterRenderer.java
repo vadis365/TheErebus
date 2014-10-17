680b8594bf63b24955c506b1f3f417ea70ddcacd
vadis365 <curve_linear@hotmail.com>
2014-10-17 22:02:12 +0100

    tweaked item render inventory

--- src/main/java/erebus/client/render/item/ItemKitchenCounterRenderer.java
+++ src/main/java/erebus/client/render/item/ItemKitchenCounterRenderer.java
@@ -8,11 +8,10 @@ import org.lwjgl.opengl.GL11;
 
 import cpw.mods.fml.client.FMLClientHandler;
 import erebus.client.model.block.ModelKitchenCounter;
-import erebus.lib.Reference;
 
-public class ItemKitchenCounterRenderer implements IItemRenderer{
-	
-	private final ResourceLocation texture = new ResourceLocation(Reference.MOD_ID, "textures/special/tiles/kitchenCounter.png");
+public class ItemKitchenCounterRenderer implements IItemRenderer {
+
+	private final ResourceLocation texture = new ResourceLocation("erebus:textures/special/tiles/kitchenCounter.png");
 	private final ModelKitchenCounter model = new ModelKitchenCounter();
 
 	@Override
@@ -21,32 +20,33 @@ public class ItemKitchenCounterRenderer implements IItemRenderer{
 	}
 
 	@Override
-	public boolean shouldUseRenderHelper(ItemRenderType type, ItemStack item, ItemRendererHelper helper) {
+	public boolean shouldUseRenderHelper(ItemRenderType type, ItemStack item,
+			ItemRendererHelper helper) {
 		return true;
 	}
 
 	@Override
 	public void renderItem(ItemRenderType type, ItemStack item, Object... data) {
 		FMLClientHandler.instance().getClient().renderEngine.bindTexture(texture);
-		switch(type){
-			case ENTITY:
-				renderBlock(0.0F, 1.0F, 0.0F);
-				break;
-			case EQUIPPED:
-				renderBlock(0.5F, 1.5F, 0.5F);
-				break;
-			case EQUIPPED_FIRST_PERSON:
-				renderBlock(0.5F, 1.0F, 0.5F);
-				break;
-			case INVENTORY:
-				renderBlock(0.0F, 1.0F, 0.0F);
-				break;
-			default:
-				break;
+		switch (type) {
+		case ENTITY:
+			renderBlock(0.0F, 1.0F, 0.0F);
+			break;
+		case EQUIPPED:
+			renderBlock(0.5F, 1.5F, 0.5F);
+			break;
+		case EQUIPPED_FIRST_PERSON:
+			renderBlock(0.5F, 1.0F, 0.5F);
+			break;
+		case INVENTORY:
+			renderInventory(0.0F, 0.625F, 0.0F);
+			break;
+		default:
+			break;
 		}
 	}
-	
-	private void renderBlock(float x, float y, float z){
+
+	private void renderBlock(float x, float y, float z) {
 		GL11.glPushMatrix();
 		GL11.glTranslatef(x, y, z);
 		GL11.glScaled(-1, -1, 1);
@@ -54,4 +54,13 @@ public class ItemKitchenCounterRenderer implements IItemRenderer{
 		GL11.glPopMatrix();
 	}
 
+	private void renderInventory(float x, float y, float z) {
+		GL11.glPushMatrix();
+		GL11.glTranslatef(x, y, z);
+		GL11.glRotatef(180F, 1F, 0, 0);
+		GL11.glRotatef(-90F, 0, 1F, 0);
+		GL11.glScalef(0.8F, 0.8F, 0.8F);
+		model.renderAll();
+		GL11.glPopMatrix();
+	}
 }
