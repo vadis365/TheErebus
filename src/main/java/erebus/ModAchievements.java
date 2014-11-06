594070a43d029f4b6ad1509a6899214f3724dee0
ganymedes01 <foka_12@hotmail.com>
2014-11-06 02:27:53 -0200

    JK didn't delete the WAILA API, just moved it to the right package
    Random formatting just so I could leave the message =P

--- src/main/java/erebus/ModAchievements.java
+++ src/main/java/erebus/ModAchievements.java
@@ -4,15 +4,16 @@ import net.minecraft.init.Items;
 import net.minecraft.stats.Achievement;
 import net.minecraftforge.common.AchievementPage;
 
-public class ModAchievements extends AchievementPage{
-	public static Achievement welcome = new Achievement("welcome", "welcome", 0, 0, ModBlocks.portal, (Achievement)null).registerStat().initIndependentStat();
+public class ModAchievements extends AchievementPage {
+
+	public static Achievement welcome = new Achievement("welcome", "welcome", 0, 0, ModBlocks.portal, (Achievement) null).registerStat().initIndependentStat();
 	public static Achievement beetle = new Achievement("beetle", "beetle", 0, -2, ModItems.food, welcome).registerStat();
 	public static Achievement beetleSpecial = new Achievement("beetleSpecial", "beetleSpecial", 1, -3, ModItems.food, welcome).registerStat().setSpecial();
 	public static Achievement diamond = new Achievement("diamond", "diamond", 2, -4, Items.diamond, beetleSpecial).registerStat();
 	public static Achievement umberstone = new Achievement("umberstone", "umberstone", -1, -2, ModBlocks.umberstone, welcome).registerStat();
 	public static Achievement tshirt = new Achievement("tshirt", "tshirt", 3, 3, ModItems.spiderTShirt, welcome).registerStat();
-	
-	public ModAchievements(){
-		super("The Erebus", new Achievement[] {welcome, beetle, beetleSpecial, diamond, umberstone, tshirt});
+
+	public ModAchievements() {
+		super("The Erebus", new Achievement[] { welcome, beetle, beetleSpecial, diamond, umberstone, tshirt });
 	}
 }
