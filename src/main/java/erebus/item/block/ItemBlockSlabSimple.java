63129349d4b324a46ad76f602df7f2b948aa0f4a
ganymedes01 <foka_12@hotmail.com>
2014-11-23 21:39:00 -0200

    Slabs are a pain... copied most of this from the vanilla class. Closes #232

--- src/main/java/erebus/item/block/ItemBlockSlabSimple.java
+++ src/main/java/erebus/item/block/ItemBlockSlabSimple.java
@@ -3,107 +3,94 @@ package erebus.item.block;
 import net.minecraft.block.Block;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.world.World;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 
-public class ItemBlockSlabSimple extends ItemBlockLocalised
-{
+public class ItemBlockSlabSimple extends ItemBlockLocalised {
 
-	public ItemBlockSlabSimple(Block block)
-	{
+	public ItemBlockSlabSimple(Block block) {
 		super(block);
 	}
 
 	@Override
-	public boolean onItemUse(ItemStack stack, EntityPlayer player, World world, int x, int y, int z, int side, float hitX, float hitY, float hitZ)
-	{
-		int meta = world.getBlockMetadata(x, y, z);
-		if (world.getBlock(x, y, z) == field_150939_a && meta != 2)
-		{
-			if (!player.canPlayerEdit(x, y, z, side, stack))
-			{
-				return false;
-			}
-
-			if (side == 1)
-			{
-				if (meta == 0 && world.checkNoEntityCollision(AxisAlignedBB.getBoundingBox(x, y, z, x + 1, y + 1, z + 1)))
-				{
-					playPlaceSound(world, x, y, z);
-					world.setBlockMetadataWithNotify(x, y, z, 2, 3);
-					stack.stackSize--;
-					return true;
-				}
-			} else if (side == 0)
-			{
-				if (meta == 1 && world.checkNoEntityCollision(AxisAlignedBB.getBoundingBox(x, y, z, x + 1, y + 1, z + 1)))
-				{
-					playPlaceSound(world, x, y, z);
-					world.setBlockMetadataWithNotify(x, y, z, 2, 3);
-					stack.stackSize--;
-					return true;
-				}
-			}
+	public boolean onItemUse(ItemStack stack, EntityPlayer player, World world, int x, int y, int z, int side, float hitX, float hitY, float hitZ) {
+		if (stack.stackSize == 0)
 			return false;
-		} else
-		{
-			int X = x;
-			int Y = y;
-			int Z = z;
-			if (side == 0)
-			{
-				Y--;
-			}
-			if (side == 1)
-			{
-				Y++;
-			}
-			if (side == 2)
-			{
-				Z--;
-			}
-			if (side == 3)
-			{
-				Z++;
-			}
-			if (side == 4)
-			{
-				X--;
-			}
-			if (side == 5)
-			{
-				X++;
-			}
+		else if (!player.canPlayerEdit(x, y, z, side, stack))
+			return false;
+		else {
+			Block block = world.getBlock(x, y, z);
+			int meta = world.getBlockMetadata(x, y, z);
+			boolean flag = meta == 1;
 
-			if (!player.canPlayerEdit(X, Y, Z, side, stack))
-			{
-				return false;
-			}
-
-			Block block = world.getBlock(X, Y, Z);
-			if (block != field_150939_a)
-			{
-				return super.onItemUse(stack, player, world, x, y, z, side, hitX, hitY, hitZ);
-			}
-
-			meta = world.getBlockMetadata(X, Y, Z);
-			boolean flag = meta != 0;
-
-			if ((side == 1 && !flag || side == 0 && flag) && block == field_150939_a)
-			{
-				if (world.checkNoEntityCollision(field_150939_a.getCollisionBoundingBoxFromPool(world, X, Y, Z)) && world.setBlock(X, Y, Z, field_150939_a, meta, 3))
-				{
-					playPlaceSound(world, X, Y, Z);
+			if ((side == 1 && !flag || side == 0 && flag) && block != field_150939_a ^ meta != 2) {
+				if (world.checkNoEntityCollision(field_150939_a.getCollisionBoundingBoxFromPool(world, x, y, z)) && world.setBlock(x, y, z, field_150939_a, 2, 3)) {
+					world.playSoundEffect(x + 0.5F, y + 0.5F, z + 0.5F, field_150939_a.stepSound.func_150496_b(), (field_150939_a.stepSound.getVolume() + 1.0F) / 2.0F, field_150939_a.stepSound.getPitch() * 0.8F);
 					stack.stackSize--;
 				}
+
 				return true;
-			}
+			} else
+				return func_150946_a(stack, player, world, x, y, z, side) ? true : super.onItemUse(stack, player, world, x, y, z, side, hitX, hitY, hitZ);
 		}
-		return false;
 	}
 
-	private void playPlaceSound(World world, int x, int y, int z)
-	{
-		world.playSoundEffect(x + 0.5F, y + 0.5F, z + 0.5F, field_150939_a.stepSound.getBreakSound(), (field_150939_a.stepSound.getVolume() + 1.0F) / 2.0F, field_150939_a.stepSound.getPitch() * 0.8F);
+	@Override
+	@SideOnly(Side.CLIENT)
+	public boolean func_150936_a(World world, int x, int y, int z, int side, EntityPlayer player, ItemStack stack) {
+		int i1 = x;
+		int j1 = y;
+		int k1 = z;
+		Block block = world.getBlock(x, y, z);
+		int meta = world.getBlockMetadata(x, y, z);
+
+		if (block == field_150939_a && meta == 2)
+			return true;
+		else {
+			if (side == 0)
+				y--;
+			if (side == 1)
+				y++;
+			if (side == 2)
+				z--;
+			if (side == 3)
+				z++;
+			if (side == 4)
+				x--;
+			if (side == 5)
+				x++;
+
+			Block block1 = world.getBlock(x, y, z);
+			return block1 == field_150939_a ? true : super.func_150936_a(world, i1, j1, k1, side, player, stack);
+		}
+	}
+
+	private boolean func_150946_a(ItemStack stack, EntityPlayer player, World world, int x, int y, int z, int side) {
+		if (side == 0)
+			y--;
+		if (side == 1)
+			y++;
+		if (side == 2)
+			z--;
+		if (side == 3)
+			z++;
+		if (side == 4)
+			x--;
+		if (side == 5)
+			x++;
+
+		Block block = world.getBlock(x, y, z);
+		int meta = world.getBlockMetadata(x, y, z);
+
+		if (block == field_150939_a && meta != 2) {
+			if (world.checkNoEntityCollision(field_150939_a.getCollisionBoundingBoxFromPool(world, x, y, z)) && world.setBlockMetadataWithNotify(x, y, z, 2, 3)) {
+				world.playSoundEffect(x + 0.5F, y + 0.5F, z + 0.5F, field_150939_a.stepSound.func_150496_b(), (field_150939_a.stepSound.getVolume() + 1.0F) / 2.0F, field_150939_a.stepSound.getPitch() * 0.8F);
+				stack.stackSize--;
+			}
+
+			return true;
+		} else
+			return false;
 	}
 }
