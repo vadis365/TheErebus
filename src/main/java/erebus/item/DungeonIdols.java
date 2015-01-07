580c792ab3f24dbccfc2fa4930918e65c932fb52
ganymedes01 <foka_12@hotmail.com>
2015-01-07 13:48:24 -0200

    Added capstones. They don't render the idols or do anything cool yet. But they work. Closes #255

--- src/main/java/erebus/item/DungeonIdols.java
+++ src/main/java/erebus/item/DungeonIdols.java
@@ -4,11 +4,9 @@ import java.util.List;
 
 import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.IIcon;
-import net.minecraft.world.World;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import erebus.ModItems;
@@ -16,10 +14,10 @@ import erebus.ModItems;
 public class DungeonIdols extends Item {
 
 	public enum IDOL {
-		idolBronze,
-		idolSilver,
-		idolGold,
-		idolJade;	
+		Bronze,
+		Silver,
+		Gold,
+		Jade;
 	}
 
 	public static ItemStack createStack(IDOL idol) {
@@ -31,31 +29,21 @@ public class DungeonIdols extends Item {
 	}
 
 	@SideOnly(Side.CLIENT)
-	public static IIcon[] icons;
+	private static IIcon[] icons;
 
 	public DungeonIdols() {
-		setHasSubtypes(true);
 		setMaxDamage(0);
-	}
-
-	@Override
-	public boolean onItemUse(ItemStack is, EntityPlayer player, World world, int x, int y, int z, int side, float hitX, float hitY, float hitZ) {
-		// TODO these will be used to activate the capstones on the dungeon pyramid
-		return false;
-	}
-
-	@Override
-	public ItemStack onItemRightClick(ItemStack is, World world, EntityPlayer player) {
-		return is;
+		setHasSubtypes(true);
+		setUnlocalizedName("erebus.idols");
 	}
 
 	@Override
 	@SideOnly(Side.CLIENT)
-	public void registerIcons(IIconRegister iconRegister) {
+	public void registerIcons(IIconRegister reg) {
 		icons = new IIcon[IDOL.values().length];
 		int i = 0;
 		for (IDOL d : IDOL.values())
-			icons[i++] = iconRegister.registerIcon("erebus:" + d.name());
+			icons[i++] = reg.registerIcon("erebus:idol" + d.name());
 	}
 
 	@Override
@@ -66,16 +54,16 @@ public class DungeonIdols extends Item {
 		return icons[meta];
 	}
 
-	@SuppressWarnings({ "unchecked", "rawtypes" })
 	@Override
 	@SideOnly(Side.CLIENT)
-	public void getSubItems(Item id, CreativeTabs tab, List list) {
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	public void getSubItems(Item item, CreativeTabs tab, List list) {
 		for (int i = 0; i < IDOL.values().length; i++)
-			list.add(new ItemStack(id, 1, i));
+			list.add(new ItemStack(item, 1, i));
 	}
 
 	@Override
-	public String getUnlocalizedName(ItemStack is) {
-		return super.getUnlocalizedName() + "." + is.getItemDamage();
+	public String getUnlocalizedName(ItemStack stack) {
+		return super.getUnlocalizedName() + "." + stack.getItemDamage();
 	}
 }
