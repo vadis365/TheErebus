fb5df7046a37e0f480765d139378a1cf19bbc5a6
vadis365 <curve_linear@hotmail.com>
2015-08-30 22:00:18 +0100

    version stuff

--- src/main/java/erebus/lib/Reference.java
+++ src/main/java/erebus/lib/Reference.java
@@ -4,7 +4,7 @@ public class Reference {
 
 	public static final String MOD_ID = "erebus";
 	public static final String MOD_NAME = "Erebus";
-	public static final String DEPENDENCIES = "required-after:Forge@[10.13.3.1360,);";
+	public static final String DEPENDENCIES = "required-after:Forge@[10.13.4.1448,);";
 	public static final String SP_CLIENT = "erebus.core.proxy.ClientProxy";
 	public static final String SP_SERVER = "erebus.core.proxy.CommonProxy";
 	public static final String CHANNEL = "erebus";
