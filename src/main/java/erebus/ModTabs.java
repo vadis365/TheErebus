00b8e0d34ce59567133fc3a5ff07dd6d8afd74b4
TheCricket <bcord@me.com>
2015-08-31 11:51:09 -0700

    optimize and fix the Creative Tabs

--- src/main/java/erebus/ModTabs.java
+++ src/main/java/erebus/ModTabs.java
@@ -1,13 +1,42 @@
 package erebus;
 
-import erebus.creativetab.*;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
 
 public class ModTabs {
 
-	public static CreativeTabs blocks = new TabBlocks();
-	public static CreativeTabs items = new TabItems();
-	public static CreativeTabs gears = new TabGears();
-	public static CreativeTabs specials = new TabSpecialItems();
-	public static CreativeTabs plants = new TabPlants();
+	public static CreativeTabs blocks = new CreativeTabs("erebus.block") {
+		@Override
+		public Item getTabIconItem() {
+			return Item.getItemFromBlock(ModBlocks.umberstone);
+		}
+	};
+
+	public static CreativeTabs items = new CreativeTabs("erebus.item") {
+		@Override
+		public Item getTabIconItem() {
+			return ModItems.food;
+		}
+	};
+
+	public static CreativeTabs gears = new CreativeTabs("erebus.gear") {
+		@Override
+		public Item getTabIconItem() {
+			return ModItems.jadePickaxe;
+		}
+	};
+
+	public static CreativeTabs specials = new CreativeTabs("erebus.special") {
+		@Override
+		public Item getTabIconItem() {
+			return ModItems.portalActivator;
+		}
+	};
+
+	public static CreativeTabs plants = new CreativeTabs("erebus.plant") {
+		@Override
+		public Item getTabIconItem() {
+			return Item.getItemFromBlock(ModBlocks.dutchCap);
+		}
+	};
 }
