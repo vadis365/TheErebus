3a0e6b30f072fb03860432aaa4d65214036bae20
vadis365 <curve_linear@hotmail.com>
2015-09-08 23:30:47 +0100

    Swamp Hut Structure
    Sneak + Right click with caveman's club to see WIP scattered structure
    feature  :P

--- src/main/java/erebus/item/ItemCavemanClub.java
+++ src/main/java/erebus/item/ItemCavemanClub.java
@@ -1,10 +1,15 @@
 package erebus.item;
 
+import java.util.Random;
+
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemSword;
+import net.minecraft.world.World;
 import erebus.ModMaterials;
 import erebus.ModTabs;
+import erebus.world.feature.structure.WorldGenSwampHut;
 
 public class ItemCavemanClub extends ItemSword {
 
@@ -17,4 +22,15 @@ public class ItemCavemanClub extends ItemSword {
 	public boolean getIsRepairable(ItemStack itemStack1, ItemStack itemStack2) {
 		return Items.bone == itemStack2.getItem() ? true : super.getIsRepairable(itemStack1, itemStack2);
 	}
+
+    // Remove onItemUse method completely after testing is over!!!!
+    @Override
+    public boolean onItemUse(ItemStack is, EntityPlayer player, World world, int x, int y, int z, int side, float hitX, float hitY, float hitZ) {
+        Random rand = new Random();
+        if( !world.isRemote && player.isSneaking() ) {
+            new WorldGenSwampHut().generateStructure(world, rand, x, y+1, z);
+            return true;
+        }
+        return false;
+    }
 }
