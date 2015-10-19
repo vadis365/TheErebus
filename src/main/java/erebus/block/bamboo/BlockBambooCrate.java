273962eb4bf868159420806a6627dd1c8c225d96
vadis365 <curve_linear@hotmail.com>
2015-10-19 21:15:59 +0100

    magically closes #278

--- src/main/java/erebus/block/bamboo/BlockBambooCrate.java
+++ src/main/java/erebus/block/bamboo/BlockBambooCrate.java
@@ -167,7 +167,6 @@ public class BlockBambooCrate extends BlockContainer {
 	}
 
 	private void resetCrates(World world, int x, int y, int z, int meta) {
-		if (meta != 0)
 			for (int i = -1; i <= 1; i++)
 				for (int j = -1; j <= 1; j++)
 					for (int k = -1; k <= 1; k++)
