1bc4bc5be3a7f75270352060bffe1af296ae15d4
vadis365 <curve_linear@hotmail.com>
2015-10-28 22:50:38 +0000

    Projectile rotation

--- src/main/java/erebus/client/render/entity/RenderEntityPreservedBlock.java
+++ src/main/java/erebus/client/render/entity/RenderEntityPreservedBlock.java
@@ -21,24 +21,24 @@ public class RenderEntityPreservedBlock extends Render {
 	}
 
 	public void renderPreservedBlock(EntityPreservedBlock entityPreservedBlock, double x, double y, double z, float yaw, float tick) {
+		float rotation = entityPreservedBlock.ticksExisted * 20 + tick;
 		GL11.glPushMatrix();
 		GL11.glTranslated(x, y, z);
+		GL11.glRotatef(rotation, 0, 1F, 0);
 		GL11.glScalef(0.5F, 0.5F, 0.5F);
 		bindTexture(TextureMap.locationBlocksTexture);
 		blockRenderer.renderBlockAsItem(ModBlocks.amber, 0, 0.6F);
 		GL11.glPopMatrix();
 		GL11.glPushMatrix();
 		GL11.glTranslated(x, y, z);
-		GL11.glRotatef(45F, 1F, 0, 0);
-		GL11.glRotatef(45F, 0, 1F, 0);
+		GL11.glRotatef(45F + rotation, 1F, 1F, 0);
 		GL11.glScalef(0.5F, 0.5F, 0.5F);
 		bindTexture(TextureMap.locationBlocksTexture);
 		blockRenderer.renderBlockAsItem(ModBlocks.amber, 0, 0.6F);
 		GL11.glPopMatrix();
 		GL11.glPushMatrix();
 		GL11.glTranslated(x, y, z);
-		GL11.glRotatef(-45F, 1F, 0, 0);
-		GL11.glRotatef(-45F, 0, 1F, 0);
+		GL11.glRotatef(-45F + rotation, 1F, 1F, 0);
 		GL11.glScalef(0.5F, 0.5F, 0.5F);
 		bindTexture(TextureMap.locationBlocksTexture);
 		blockRenderer.renderBlockAsItem(ModBlocks.amber, 0, 0.6F);
