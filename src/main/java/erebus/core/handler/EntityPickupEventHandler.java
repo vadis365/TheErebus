5af598b0151c6b4d94a667f576814b9e8a6b8872
ganymedes01 <foka_12@hotmail.com>
2015-11-01 13:13:41 +0000

    Dat cricket code

--- src/main/java/erebus/core/handler/EntityPickupEventHandler.java
+++ src/main/java/erebus/core/handler/EntityPickupEventHandler.java
@@ -1,16 +1,15 @@
 package erebus.core.handler;
 
-import net.minecraft.item.ItemStack;
 import net.minecraftforge.event.entity.player.EntityItemPickupEvent;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import erebus.ModAchievements;
 import erebus.ModItems;
 
 public class EntityPickupEventHandler {
-	
+
 	@SubscribeEvent
 	public void itemPickup(EntityItemPickupEvent event) {
-		if (event.item.getEntityItem() == new ItemStack(ModItems.spiderTShirt))
+		if (event.item.getEntityItem().getItem() == ModItems.spiderTShirt)
 			event.entityPlayer.triggerAchievement(ModAchievements.tshirt);
 	}
 }
