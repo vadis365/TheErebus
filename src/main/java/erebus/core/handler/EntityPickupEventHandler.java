fee507f54101394a5cd28f43026662146f0d26f6
ganymedes01 <foka_12@hotmail.com>
2015-11-01 13:18:23 +0000

    Closes #407

--- src/main/java/erebus/core/handler/EntityPickupEventHandler.java
+++ src/main/java/erebus/core/handler/EntityPickupEventHandler.java
@@ -1,15 +1,29 @@
 package erebus.core.handler;
 
+import net.minecraft.block.Block;
+import net.minecraft.item.ItemStack;
+import net.minecraft.stats.AchievementList;
 import net.minecraftforge.event.entity.player.EntityItemPickupEvent;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import erebus.ModAchievements;
+import erebus.ModBlocks;
 import erebus.ModItems;
+import erebus.block.BlockLogErebus;
 
 public class EntityPickupEventHandler {
 
 	@SubscribeEvent
 	public void itemPickup(EntityItemPickupEvent event) {
-		if (event.item.getEntityItem().getItem() == ModItems.spiderTShirt)
+		ItemStack stack = event.item.getEntityItem();
+		if (stack == null || stack.getItem() == null)
+			return;
+
+		if (stack.getItem() == ModItems.spiderTShirt)
 			event.entityPlayer.triggerAchievement(ModAchievements.tshirt);
+		else {
+			Block block = Block.getBlockFromItem(stack.getItem());
+			if (block instanceof BlockLogErebus || block == ModBlocks.scorchedWood || block == ModBlocks.saplessLog)
+				event.entityPlayer.triggerAchievement(AchievementList.mineWood);
+		}
 	}
 }
