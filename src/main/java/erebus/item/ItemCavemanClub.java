6958531239fc1d04fe9a93f37948d237f73ef421
ganymedes01 <foka_12@hotmail.com>
2015-11-17 16:14:56 +0000

    Remove debug stuff from the caveman's club

--- src/main/java/erebus/item/ItemCavemanClub.java
+++ src/main/java/erebus/item/ItemCavemanClub.java
@@ -1,15 +1,10 @@
 package erebus.item;
 
-import java.util.Random;
-
 import erebus.ModMaterials;
 import erebus.ModTabs;
-import erebus.world.feature.decoration.WorldGenAmberGround;
-import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemSword;
-import net.minecraft.world.World;
 
 public class ItemCavemanClub extends ItemSword {
 
@@ -19,19 +14,7 @@ public class ItemCavemanClub extends ItemSword {
 	}
 
 	@Override
-	public boolean getIsRepairable(ItemStack itemStack1, ItemStack itemStack2) {
-		return Items.bone == itemStack2.getItem() ? true : super.getIsRepairable(itemStack1, itemStack2);
-	}
-
-	// Remove onItemUse method completely after testing is over!!!!
-	@Override
-	public boolean onItemUse(ItemStack is, EntityPlayer player, World world, int x, int y, int z, int side, float hitX, float hitY, float hitZ) {
-		Random rand = new Random();
-		if (!world.isRemote && player.isSneaking()) {
-			new WorldGenAmberGround().generate(world, rand, x, y + 1, z);
-			return true;
-		}
-
-		return false;
+	public boolean getIsRepairable(ItemStack tool, ItemStack material) {
+		return Items.bone == material.getItem() ? true : super.getIsRepairable(tool, material);
 	}
 }
