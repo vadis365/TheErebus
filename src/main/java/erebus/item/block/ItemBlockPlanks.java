3b366fad910984b782c7a2255958d55bcb98c190
ganymedes01 <foka_12@hotmail.com>
2015-11-18 19:19:33 +0000

    Return planks icon from the respective ItemBlock

--- src/main/java/erebus/item/block/ItemBlockPlanks.java
+++ src/main/java/erebus/item/block/ItemBlockPlanks.java
@@ -1,9 +1,12 @@
 package erebus.item.block;
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import erebus.lib.EnumWood;
 import erebus.lib.Reference;
 import net.minecraft.block.Block;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.IIcon;
 import net.minecraft.util.StatCollector;
 
 public class ItemBlockPlanks extends ItemBlockGeneric {
@@ -19,5 +22,11 @@ public class ItemBlockPlanks extends ItemBlockGeneric {
 			return "Error";
 		EnumWood wood = EnumWood.values()[meta];
 		return StatCollector.translateToLocal("tile." + Reference.MOD_ID + ".planks_" + wood.getUnlocalisedName() + ".name");
+	}
+
+	@Override
+	@SideOnly(Side.CLIENT)
+	public IIcon getIconFromDamage(int meta) {
+		return getBlock().getIcon(0, meta);
 	}
 }
