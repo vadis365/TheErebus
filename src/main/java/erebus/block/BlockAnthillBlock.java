ce7b11460e8bd165f531c2c1c9e2b4f1fa1180c6
ganymedes01 <foka_12@hotmail.com>
2015-11-30 19:38:47 +0000

    Change anthill block to sound like stone and not grass

--- src/main/java/erebus/block/BlockAnthillBlock.java
+++ src/main/java/erebus/block/BlockAnthillBlock.java
@@ -1,18 +1,18 @@
 package erebus.block;
 
+import erebus.ModTabs;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
-import erebus.ModTabs;
 
 public class BlockAnthillBlock extends Block {
 
 	public BlockAnthillBlock() {
 		super(Material.rock);
-		setCreativeTab(ModTabs.blocks);
-		setStepSound(Block.soundTypeGrass);
-		setBlockTextureName("erebus:anthillBlock");
-		setBlockName("erebus.anthillBlock");
 		setBlockUnbreakable();
 		setResistance(6000000.0F);
+		setStepSound(soundTypeStone);
+		setCreativeTab(ModTabs.blocks);
+		setBlockName("erebus.anthillBlock");
+		setBlockTextureName("erebus:anthillBlock");
 	}
 }
