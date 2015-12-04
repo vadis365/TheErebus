988c8728db3c9c466f287cc59e151c015ed77ee0
vadis365 <curve_linear@hotmail.com>
2015-12-04 21:59:09 +0000

    ant trap damages player based on direction

--- src/main/java/erebus/tileentity/TileEntitySoldierAntTrap.java
+++ src/main/java/erebus/tileentity/TileEntitySoldierAntTrap.java
@@ -1,13 +1,20 @@
 package erebus.tileentity;
 
+import java.util.List;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.network.NetworkManager;
 import net.minecraft.network.Packet;
 import net.minecraft.network.play.server.S35PacketUpdateTileEntity;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.AxisAlignedBB;
+import net.minecraft.util.DamageSource;
 
 public class TileEntitySoldierAntTrap extends TileEntity {
-	
+
 	public int animationTicks;
 	public boolean active;
 
@@ -19,9 +26,10 @@ public class TileEntitySoldierAntTrap extends TileEntity {
 	@Override
 	public void updateEntity() {
 		if (!worldObj.isRemote) {
+			findEnemyToAttack();
 			if (active) {
 				if (animationTicks == 0)
-					worldObj.playSoundEffect(xCoord, yCoord, zCoord, "mob.zombie.say", 1.0F, 0.5F);
+					worldObj.playSoundEffect(xCoord, yCoord, zCoord, "mob.zombie.say", 0.25F, 0.5F);
 				if (animationTicks == 11)
 					worldObj.playSoundEffect(xCoord, yCoord, zCoord, "mob.sheep.shear", 2.0F, 0.5F);
 				if (animationTicks <= 16)
@@ -39,11 +47,34 @@ public class TileEntitySoldierAntTrap extends TileEntity {
 			worldObj.markBlockForUpdate(xCoord, yCoord, zCoord);
 		}
 	}
-	
+
 	public void setActive(boolean isActive) {
 		active = isActive;
 	}
-	
+
+	@SuppressWarnings("unchecked")
+	protected Entity findEnemyToAttack() {
+		int meta = getBlockMetadata();
+		float x = 0, z = 0;
+		if(meta == 4)
+			x = -1F/16 * animationTicks;
+		if(meta == 5)
+			x = 1F/16 * animationTicks;
+		if(meta == 2)
+			z = -1F/16 * animationTicks;
+		if(meta == 3)
+			z = 1F/16 * animationTicks;
+		List<EntityLivingBase> list = worldObj.getEntitiesWithinAABB(EntityLivingBase.class, AxisAlignedBB.getBoundingBox(xCoord + x, yCoord, zCoord + z, xCoord + 1D + x, yCoord + 1D, zCoord + 1D + z));
+		if (animationTicks >= 1)
+			for (int i = 0; i < list.size(); i++) {
+				Entity entity = list.get(i);
+				if (entity != null)
+					if (entity instanceof EntityPlayer)
+						((EntityLivingBase) entity).attackEntityFrom(DamageSource.generic , 2);
+			}
+		return null;
+	}
+
 	@Override
 	public void writeToNBT(NBTTagCompound nbt) {
 		super.writeToNBT(nbt);
