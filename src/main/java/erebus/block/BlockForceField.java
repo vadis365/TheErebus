0b350d515a35b0c53683ce54b320911307f3c66f
vadis365 <curve_linear@hotmail.com>
2015-12-10 22:36:26 +0000

    some force field/lock bits

--- src/main/java/erebus/block/BlockForceField.java
+++ src/main/java/erebus/block/BlockForceField.java
@@ -5,6 +5,7 @@ import java.util.Random;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import erebus.Erebus;
+import erebus.ModBlocks;
 import erebus.ModTabs;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
@@ -36,7 +37,7 @@ public class BlockForceField extends Block {
 	@SideOnly(Side.CLIENT)
 	@Override
 	public boolean shouldSideBeRendered(IBlockAccess world, int x, int y, int z, int side) {
-		return world.getBlock(x, y, z) == this ? false : super.shouldSideBeRendered(world, x, y, z, side);
+		return world.getBlock(x, y, z) == this || world.getBlock(x, y, z) == ModBlocks.forceLock ? false : super.shouldSideBeRendered(world, x, y, z, side);
 	}
 
 	@SideOnly(Side.CLIENT)
--- src/main/java/erebus/block/BlockForceLock.java
+++ src/main/java/erebus/block/BlockForceLock.java
@@ -1,16 +1,33 @@
 package erebus.block;
 
+import java.util.Random;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.AxisAlignedBB;
+import net.minecraft.util.DamageSource;
+import net.minecraft.util.IIcon;
+import net.minecraft.util.MathHelper;
+import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.World;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import erebus.Erebus;
+import erebus.ModBlocks;
 import erebus.ModItems;
 import erebus.ModTabs;
 import erebus.core.helper.Utils;
 import erebus.item.ItemMaterials;
-import net.minecraft.block.Block;
-import net.minecraft.block.material.Material;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.item.ItemStack;
-import net.minecraft.world.World;
 
 public class BlockForceLock extends Block {
+
+	@SideOnly(Side.CLIENT)
+	private IIcon sides, front;
 
 	public BlockForceLock() {
 		super(Material.rock);
@@ -19,7 +36,41 @@ public class BlockForceLock extends Block {
 		setStepSound(soundTypeStone);
 		setCreativeTab(ModTabs.blocks);
 		setBlockName("erebus.forceLock");
-		setBlockTextureName("erebus:anthillBlock");
+	}
+
+	@Override
+	@SideOnly(Side.CLIENT)
+	public IIcon getIcon(int side, int meta) {
+		return meta == 0 && side == 4 ? front : meta == 1 && side == 2 ? front : meta == 2 && side == 5 ? front : meta == 3 && side == 3 ? front : blockIcon;
+	}
+
+	@Override
+	@SideOnly(Side.CLIENT)
+	public void registerBlockIcons(IIconRegister reg) {
+		blockIcon = reg.registerIcon("erebus:forceField");
+		front = reg.registerIcon("erebus:force_lock");
+	}
+
+	@SideOnly(Side.CLIENT)
+	@Override
+	public boolean shouldSideBeRendered(IBlockAccess world, int x, int y, int z, int side) {
+		return world.getBlock(x, y, z) == ModBlocks.forceField ? false : super.shouldSideBeRendered(world, x, y, z, side);
+	}
+
+	@SideOnly(Side.CLIENT)
+	@Override
+	public int getRenderBlockPass() {
+		return 1;
+	}
+
+	@Override
+	public boolean isOpaqueCube() {
+		return false;
+	}
+
+	@Override
+	public boolean renderAsNormalBlock() {
+		return false;
 	}
 
 	@Override
@@ -32,7 +83,61 @@ public class BlockForceLock extends Block {
 				Utils.breakBlockWithParticles(world, x, y, z, 0);
 			return true;
 		}
-
 		return false;
+	}
+
+	@Override
+	public void onBlockPlacedBy(World world, int x, int y, int z, EntityLivingBase player, ItemStack is) {
+		int l = MathHelper.floor_double(player.rotationYaw * 4.0F / 360.0F + 1.5D) & 3;
+		world.setBlockMetadataWithNotify(x, y, z, l, 2);
+	}
+
+	@Override
+	public void randomDisplayTick(World world, int x, int y, int z, Random rand) {
+		Random random = world.rand;
+		double pixel = 0.0625D;
+		if (rand.nextInt(5) == 0)
+			for (int l = 0; l < 6; ++l) {
+				double particleX = x + random.nextFloat();
+				double particleY = y + random.nextFloat();
+				double particleZ = z + random.nextFloat();
+
+				if (l == 0 && !world.getBlock(x, y + 1, z).isOpaqueCube())
+					particleY = y + 1 + pixel;
+
+				if (l == 1 && !world.getBlock(x, y - 1, z).isOpaqueCube())
+					particleY = y - pixel;
+
+				if (l == 2 && !world.getBlock(x, y, z + 1).isOpaqueCube())
+					particleZ = z + 1 + pixel;
+
+				if (l == 3 && !world.getBlock(x, y, z - 1).isOpaqueCube())
+					particleZ = z - pixel;
+
+				if (l == 4 && !world.getBlock(x + 1, y, z).isOpaqueCube())
+					particleX = x + 1 + pixel;
+
+				if (l == 5 && !world.getBlock(x - 1, y, z).isOpaqueCube())
+					particleX = x - pixel;
+
+				if (particleX < x || particleX > x + 1 || particleY < 0.0D || particleY > y + 1 || particleZ < z || particleZ > z + 1)
+					Erebus.proxy.spawnCustomParticle("sparks", world, particleX, particleY, particleZ, 0, 0, 0);
+			}
+	}
+
+	@Override
+	public AxisAlignedBB getCollisionBoundingBoxFromPool(World world, int x, int y, int z) {
+		double twoPixels = 0.125D;
+		return AxisAlignedBB.getBoundingBox(x + twoPixels, y, z + twoPixels, x + 1 - twoPixels, y + 1, z + 1 - twoPixels);
+	}
+
+	@Override
+	public void onEntityCollidedWithBlock(World world, int x, int y, int z, Entity entity) {
+		if (entity instanceof EntityLivingBase) {
+			int Knockback = 1;
+			entity.attackEntityFrom(DamageSource.cactus, 1);
+			entity.addVelocity(MathHelper.sin(entity.rotationYaw * 3.141593F / 180.0F) * Knockback * 0.1F, 0.08D, -MathHelper.cos(entity.rotationYaw * 3.141593F / 180.0F) * Knockback * 0.1F);
+			entity.worldObj.playSoundAtEntity(entity, "erebus:glowwormhurt", 1.0F, 1.0F);
+		}
 	}
 }
