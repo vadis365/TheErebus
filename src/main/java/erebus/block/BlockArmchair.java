36781b5fe44ff0979b3248461f10ab46fe17e0d0
vadis365 <curve_linear@hotmail.com>
2015-12-22 23:24:45 +0000

    stupid format stuff

--- src/main/java/erebus/block/BlockArmchair.java
+++ src/main/java/erebus/block/BlockArmchair.java
@@ -104,13 +104,13 @@ public class BlockArmchair extends BlockContainer {
 			player.getEntityData().setInteger("armchairX", (int) x);
 			player.getEntityData().setInteger("armchairY", (int) y);
 			player.getEntityData().setInteger("armchairZ", (int) z);
-        		player.getEntityData().setBoolean("armchairSpawn", true);
-        		Erebus.proxy.getClientPlayer().addChatComponentMessage(new ChatComponentText(StatCollector.translateToLocal("armchair.spawnSet")));
-        		entityMountableBlock = new EntityArmchairMount(world);
-        		entityMountableBlock.setPosition(x + 0.5D, y  + 0.5D, z  + 0.5D);
-        		entityMountableBlock.setChairAngle(meta);
-        		world.spawnEntityInWorld(entityMountableBlock);
-        		player.mountEntity(entityMountableBlock);
+			player.getEntityData().setBoolean("armchairSpawn", true);
+			Erebus.proxy.getClientPlayer().addChatComponentMessage(new ChatComponentText(StatCollector.translateToLocal("armchair.spawnSet")));
+			entityMountableBlock = new EntityArmchairMount(world);
+			entityMountableBlock.setPosition(x + 0.5D, y  + 0.5D, z  + 0.5D);
+			entityMountableBlock.setChairAngle(meta);
+			world.spawnEntityInWorld(entityMountableBlock);
+			player.mountEntity(entityMountableBlock);
 		}
 	}
         return true;
