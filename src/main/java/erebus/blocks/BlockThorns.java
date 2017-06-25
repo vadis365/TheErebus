ac7d5953e45c85e7308ed85a8c3abfaa8bde63e8
vadis365 <curve_linear@hotmail.com>
2017-06-25 02:47:04 +0100

    start of thorns
    time for sleep...

--- src/main/java/erebus/blocks/BlockThorns.java
+++ src/main/java/erebus/blocks/BlockThorns.java
@@ -1,13 +1,13 @@
-package erebus.block.plants;
+package erebus.blocks;
 
-import cpw.mods.fml.relauncher.Side;
-import cpw.mods.fml.relauncher.SideOnly;
 import erebus.ModTabs;
-import net.minecraft.block.Block;
 import net.minecraft.block.BlockVine;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.util.DamageSource;
-import net.minecraft.world.IBlockAccess;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraftforge.common.IShearable;
 
@@ -16,20 +16,14 @@ public class BlockThorns extends BlockVine implements IShearable {
 	public BlockThorns() {
 		super();
 		setHardness(0.2F);
-		setBlockName("erebus.thorns");
-		setCreativeTab(ModTabs.plants);
-		setStepSound(Block.soundTypeGrass);
-		setBlockTextureName("erebus:thorns");
+		setCreativeTab(ModTabs.PLANTS);
+		setSoundType(SoundType.PLANT);
 	}
 
 	@Override
-	public void onEntityCollidedWithBlock(World world, int x, int y, int z, Entity entity) {
-		entity.attackEntityFrom(DamageSource.cactus, 1);
+	public void onEntityCollidedWithBlock(World worldIn, BlockPos pos, IBlockState state, Entity entity) {
+		if (!worldIn.isRemote && entity instanceof EntityLivingBase)
+			entity.attackEntityFrom(DamageSource.CACTUS, 1);
 	}
 
-	@Override
-	@SideOnly(Side.CLIENT)
-	public int colorMultiplier(IBlockAccess access, int x, int y, int z) {
-		return 0xFFFFFF;
-	}
 }
