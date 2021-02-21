1bc7d59ac76fcfe779f2e33108f85b111f448cbc
vadis365 <curve_linear@hotmail.com>
2021-02-21 18:28:14 +0000

    small version bump

--- src/main/java/erebus/lib/Reference.java
+++ src/main/java/erebus/lib/Reference.java
@@ -4,7 +4,7 @@ public class Reference {
 
 	public static final String MOD_ID = "erebus";
 	public static final String MOD_NAME = "Erebus";
-	public static final String VERSION = "1.0.31";
+	public static final String VERSION = "1.0.32";
 	public static final String PROXY_CLIENT = "erebus.proxy.ClientProxy";
 	public static final String PROXY_COMMON = "erebus.proxy.CommonProxy";
 	public static final String GUI_FACTORY_CLASS = "erebus.core.handler.configs.ConfigGuiFactory";
