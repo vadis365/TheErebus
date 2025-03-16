03a814ff637e5bd69394bdc842a5564b7bc52794
vadis365 <6518708+vadis365@users.noreply.github.com>
2025-03-16 16:59:14 +0000

    Animated block stuff WIP

--- src/main/java/erebus/entity/AnimatedBlock.java
+++ src/main/java/erebus/entity/AnimatedBlock.java
@@ -18,16 +18,23 @@ import net.minecraft.world.entity.SpawnGroupData;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.goal.FloatGoal;
+import net.minecraft.world.entity.ai.goal.MeleeAttackGoal;
 import net.minecraft.world.entity.ai.goal.WaterAvoidingRandomStrollGoal;
+import net.minecraft.world.entity.ai.goal.target.HurtByTargetGoal;
+import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
 import net.minecraft.world.entity.monster.Monster;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.ServerLevelAccessor;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.api.distmarker.OnlyIn;
 
 public class AnimatedBlock extends PathfinderMob {
 
 	private static final EntityDataAccessor<BlockState> BLOCK_TYPE = SynchedEntityData.defineId(AnimatedBlock.class, EntityDataSerializers.BLOCK_STATE);
+	private int lastX = 0, lastY = 0, lastZ = 0;
 
 	public AnimatedBlock (EntityType<? extends AnimatedBlock> type, Level level) { 
 		super(type, level);
@@ -47,17 +54,59 @@ public class AnimatedBlock extends PathfinderMob {
 	public BlockState getBlockType() {
 		return entityData.get(BLOCK_TYPE);
 	}
+
 	@Override
 	protected void registerGoals() {
 		goalSelector.addGoal(0, new FloatGoal(this));
-		goalSelector.addGoal(1, new WaterAvoidingRandomStrollGoal(this, 0.5D, 1));
+		goalSelector.addGoal(1, new MeleeAttackGoal(this, 0.5D, false));
+		goalSelector.addGoal(2, new WaterAvoidingRandomStrollGoal(this, 0.5D, 1));
+		targetSelector.addGoal(0, new HurtByTargetGoal(this));
+		targetSelector.addGoal(1, new NearestAttackableTargetGoal<Monster>(this, Monster.class, true, true));
 	}
 
 	public static AttributeSupplier.Builder createAttributes() {
-		return Monster.createMonsterAttributes().add(Attributes.MAX_HEALTH, 1D)
-				.add(Attributes.FOLLOW_RANGE, 32D)
+		return Monster.createMonsterAttributes().add(Attributes.MAX_HEALTH, 10D)
+				.add(Attributes.FOLLOW_RANGE, 16D)
 				.add(Attributes.MOVEMENT_SPEED, 0.5D)
-				.add(Attributes.ATTACK_DAMAGE, 0);
+				.add(Attributes.ATTACK_DAMAGE, 2D);
+	}
+
+	@SuppressWarnings("deprecation")
+	@Override
+	public void tick() {
+		super.tick();
+		if (level().isClientSide() && isGlowingBlock(getBlockType()))
+			if (getLightLevelDependentMagicValue() < 0.5F)
+				lightUp(level(), blockPosition());
+			else
+				switchOff();
+	}
+
+	@SuppressWarnings("deprecation")
+	private boolean isGlowingBlock(BlockState state) {
+		return state.getLightEmission() > 0;
+	}
+
+	@OnlyIn(Dist.CLIENT)
+	private void lightUp(Level level, BlockPos pos) {
+	//TODO  work out wtf this is in the Lighting code!!!
+		//level.setLightFor(LightLayer.BLOCK, pos, 9);
+		for (int i = -2; i < 2; i++)
+			for (int j = -2; j < 2; j++)
+				for (int k = -2; k < 2; k++)
+					if (pos.getX() + i != lastX || pos.getY() + j != lastY || pos.getZ() + k != lastZ || isRemoved()) {
+						level.getChunkSource().getLightEngine().checkBlock(new BlockPos(lastX + i, lastY + j, lastZ + k));
+						//level.checkLightFor(LightLayer.BLOCK, new BlockPos(lastX + i, lastY + j, lastZ + k));
+						lastX = pos.getX();
+						lastY = pos.getY();
+						lastZ = pos.getZ();
+					}
+	}
+
+	@OnlyIn(Dist.CLIENT)
+	private void switchOff() {
+		level().getChunkSource().getLightEngine().checkBlock(new BlockPos(lastX, lastY, lastZ));
+		level().getChunkSource().getLightEngine().checkBlock(blockPosition());
 	}
 
 	@Override
@@ -65,12 +114,15 @@ public class AnimatedBlock extends PathfinderMob {
         this.playSound(SoundEvents.SPIDER_STEP, 0.15F, 1.0F);
     }
 
+	@SuppressWarnings("deprecation")
 	@Override
 	public void kill() {
 		super.kill();
-		level().setBlock(blockPosition(), getBlockType(), 3);
+		spawnAtLocation(Item.byBlock(getBlockType().getBlock()), 1);
+		if (level().isClientSide() && isGlowingBlock(getBlockType()))
+			switchOff();
 	}
-	
+
 	@Nullable
 	@Override
 	public SpawnGroupData finalizeSpawn(ServerLevelAccessor level, DifficultyInstance difficulty, MobSpawnType spawnType, @Nullable SpawnGroupData spawnGroupData) {
