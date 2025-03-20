f6b35a839163110502066e7557f7a0eb4646f3a1
vadis365 <6518708+vadis365@users.noreply.github.com>
2025-03-20 17:56:33 +0000

    Start of Scytodes WIP
    just the model file atm

--- src/main/java/erebus/registries/ModEntityRendering.java
+++ src/main/java/erebus/registries/ModEntityRendering.java
@@ -2,6 +2,7 @@ package erebus.registries;
 
 import erebus.Erebus;
 import erebus.client.render.entity.model.AnimatedBlockModel;
+import erebus.client.render.entity.model.ScytodesModel;
 import erebus.client.render.entity.model.WaspModel;
 import erebus.client.render.entity.renderer.AnimatedBlockRenderer;
 import erebus.client.render.entity.renderer.WaspRenderer;
@@ -13,11 +14,14 @@ public class ModEntityRendering {
     public static ModelLayerLocation GRASHOPPER = new ModelLayerLocation(Erebus.prefix("grasshopper"), "main");
     public static ModelLayerLocation WASP = new ModelLayerLocation(Erebus.prefix("wasp"), "main");
     public static ModelLayerLocation ANIMATED_BLOCK = new ModelLayerLocation(Erebus.prefix("animated_block"), "main");
+	public static ModelLayerLocation SCYTODES = new ModelLayerLocation(Erebus.prefix("scytodes"), "main");
 
     public static void registerEntityLayers(EntityRenderersEvent.RegisterLayerDefinitions event) {
-      //  event.registerLayerDefinition(GRASHOPPER, ModelGrasshopper::createBodyLayer);
+    	//  event.registerLayerDefinition(GRASHOPPER, ModelGrasshopper::createBodyLayer);
+    	// event.registerLayerDefinition(SCYTODES, ScytodesModel::createBodyLayer);
     	event.registerLayerDefinition(WASP, WaspModel::createBodyLayer);
-    	 event.registerLayerDefinition(ANIMATED_BLOCK, AnimatedBlockModel::createBodyLayer);
+    	event.registerLayerDefinition(ANIMATED_BLOCK, AnimatedBlockModel::createBodyLayer);
+    	
     }
 
     public static void registerEntityRender(EntityRenderersEvent.RegisterRenderers event) {
