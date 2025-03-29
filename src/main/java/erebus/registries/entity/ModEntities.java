470a78fa89b771f0cccd0bc2a20860a433c9b4c8
vadis365 <6518708+vadis365@users.noreply.github.com>
2025-03-29 17:54:56 +0000

    hnnngnnnnhnnnnhnngnnngngnnnnhhh

--- src/main/java/erebus/registries/entity/ModEntities.java
+++ src/main/java/erebus/registries/entity/ModEntities.java
@@ -40,7 +40,7 @@ public class ModEntities {
     public static final DeferredHolder<EntityType<?>, EntityType<Wasp>> WASP = registerWithEgg("wasp", EntityType.Builder.of(Wasp::new, MobCategory.MONSTER).sized(0.5F, 0.4F), 0xFECD09, 0x141414);
     public static final DeferredHolder<EntityType<?>, EntityType<AnimatedBlock>> ANIMATED_BLOCK = registerNoEgg("animated_block", EntityType.Builder.of(AnimatedBlock::new, MobCategory.MISC).fireImmune().sized(1F, 1.25F).clientTrackingRange(4).updateInterval(10));
     public static final DeferredHolder<EntityType<?>, EntityType<Scytodes>> SCYTODES = registerWithEgg("scytodes", EntityType.Builder.of(Scytodes::new, MobCategory.MONSTER).sized(2F, 1F).fireImmune(), 0xC2833C, 0x520D06);
-    public static final DeferredHolder<EntityType<?>, EntityType<MoneySpider>> MONEY_SPIDER = registerWithEgg("money_spider", EntityType.Builder.of(MoneySpider::new, MobCategory.MONSTER).sized(0.6F, 0.4F), 0xC2872F, 0xF9FF00);
+    public static final DeferredHolder<EntityType<?>, EntityType<MoneySpider>> MONEY_SPIDER = registerWithEgg("money_spider", EntityType.Builder.of(MoneySpider::new, MobCategory.MONSTER).sized(0.6F, 0.4F).fireImmune(), 0xC2872F, 0xF9FF00);
     public static final DeferredHolder<EntityType<?>, EntityType<BlackWidow>> BLACK_WIDOW = registerWithEgg("black_widow", EntityType.Builder.of(BlackWidow::new, MobCategory.MONSTER).sized(0.7F, 0.325F).fireImmune(), 0x101010, 0xFF0000);
     public static final DeferredHolder<EntityType<?>, EntityType<LavaWebSpider>> LAVA_WEB_SPIDER = registerWithEgg("lava_web_spider", EntityType.Builder.of(LavaWebSpider::new, MobCategory.MONSTER).sized(3F, 1.5F).fireImmune(), 0xD36617, 0x342522);
 	
