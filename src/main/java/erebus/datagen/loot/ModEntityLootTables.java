1be586b900dca20ffbb6fb7dffb4f3c57270e836
vadis365 <6518708+vadis365@users.noreply.github.com>
2025-03-29 17:50:44 +0000

    start of entity loot table insanity

--- src/main/java/erebus/datagen/loot/ModEntityLootTables.java
+++ src/main/java/erebus/datagen/loot/ModEntityLootTables.java
@@ -1,13 +1,22 @@
 package erebus.datagen.loot;
 
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
+import erebus.registries.ModItems;
 import erebus.registries.entity.ModEntities;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.loot.EntityLootSubProvider;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.flag.FeatureFlags;
-
-import java.util.function.Supplier;
-import java.util.stream.Stream;
+import net.minecraft.world.level.storage.loot.LootPool;
+import net.minecraft.world.level.storage.loot.LootTable;
+import net.minecraft.world.level.storage.loot.entries.LootItem;
+import net.minecraft.world.level.storage.loot.functions.EnchantedCountIncreaseFunction;
+import net.minecraft.world.level.storage.loot.functions.SetItemCountFunction;
+import net.minecraft.world.level.storage.loot.providers.number.UniformGenerator;
+import net.neoforged.neoforge.registries.DeferredHolder;
 
 public class ModEntityLootTables extends EntityLootSubProvider {
 
@@ -17,8 +26,26 @@ public class ModEntityLootTables extends EntityLootSubProvider {
 
     @Override
     public void generate() {
+    	//TODO work out how TF this all works now
+    	//this.noLoot(ModEntities.ANIMATED_BLOCK);
+    	
+		this.add(ModEntities.BLACK_WIDOW.get(), LootTable.lootTable()
+				.withPool(LootPool.lootPool()
+					.add(LootItem.lootTableItem(ModItems.POISON_GLAND).apply(SetItemCountFunction.setCount(UniformGenerator.between(1, 2)))
+							.apply(EnchantedCountIncreaseFunction.lootingMultiplier(this.registries, UniformGenerator.between(0, 1))))));
 
+		this.noLoot(ModEntities.LAVA_WEB_SPIDER);
+		
+		this.noLoot(ModEntities.SCYTODES);
+		
+		this.noLoot(ModEntities.MONEY_SPIDER);
+
+		this.noLoot(ModEntities.WASP);
     }
+    
+	public <T extends Entity> void noLoot(DeferredHolder<EntityType<?>, EntityType<T>> type) {
+		this.add(type.get(), LootTable.lootTable());
+	}
 
     @Override
     protected Stream<EntityType<?>> getKnownEntityTypes() {
--- src/main/java/erebus/datagen/providers/ModLootTableProvider.java
+++ src/main/java/erebus/datagen/providers/ModLootTableProvider.java
@@ -1,5 +1,9 @@
 package erebus.datagen.providers;
 
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.CompletableFuture;
+
 import erebus.datagen.loot.ModBlockLootTables;
 import erebus.datagen.loot.ModChestLootTables;
 import erebus.datagen.loot.ModEntityLootTables;
@@ -8,17 +12,13 @@ import net.minecraft.data.PackOutput;
 import net.minecraft.data.loot.LootTableProvider;
 import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
 
-import java.util.List;
-import java.util.Set;
-import java.util.concurrent.CompletableFuture;
-
 public class ModLootTableProvider extends LootTableProvider {
 
     public ModLootTableProvider(PackOutput output, CompletableFuture<HolderLookup.Provider> registries) {
         super(output, Set.of(), List.of(
                 new SubProviderEntry(ModBlockLootTables::new, LootContextParamSets.BLOCK),
-                new SubProviderEntry(ModChestLootTables::new, LootContextParamSets.CHEST)
-                // new SubProviderEntry(ModEntityLootTables::new, LootContextParamSets.ENTITY)
+                new SubProviderEntry(ModChestLootTables::new, LootContextParamSets.CHEST),
+                new SubProviderEntry(ModEntityLootTables::new, LootContextParamSets.ENTITY)
         ), registries);
     }
 }
