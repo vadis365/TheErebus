d9f91c0e583ccde651a4ec2278f2aba778ac4b88
Cricket <cricke@chirp.space>
2025-04-22 22:35:19 -0400

    Setup more ground work for structures
    Signed-off-by: Cricket <cricke@chirp.space>

--- src/main/java/erebus/datagen/ModRegistries.java
+++ src/main/java/erebus/datagen/ModRegistries.java
@@ -4,6 +4,7 @@ import erebus.Erebus;
 import erebus.registries.world.ModBiomes;
 import erebus.registries.world.ModDimensionRegistries;
 import erebus.registries.world.ModFeatures;
+import erebus.registries.world.ModStructures;
 import erebus.world.ModNoiseGenerator;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.core.RegistrySetBuilder;
@@ -22,7 +23,11 @@ public class ModRegistries extends DatapackBuiltinEntriesProvider {
             .add(Registries.PLACED_FEATURE, ModFeatures::bootstrapPlacedFeatures)
             .add(Registries.DIMENSION_TYPE, ModDimensionRegistries::bootstrapType)
             .add(Registries.NOISE_SETTINGS, ModNoiseGenerator::bootstrap)
-            .add(Registries.LEVEL_STEM, ModDimensionRegistries::bootstrapStem);
+            .add(Registries.LEVEL_STEM, ModDimensionRegistries::bootstrapStem)
+            .add(Registries.STRUCTURE, ModStructures::bootstrapStructures)
+            .add(Registries.STRUCTURE_SET, ModStructures::bootstrapSets)
+            .add(Registries.TEMPLATE_POOL, ModStructures::bootstrapPools)
+            .add(Registries.PROCESSOR_LIST, ModStructures::bootstrapProcessors);
 
     public ModRegistries(PackOutput output, CompletableFuture<HolderLookup.Provider> future) {
         super(output, future, BUILDER, Set.of("minecraft", Erebus.MODID));
--- src/main/java/erebus/registries/world/ModStructures.java
+++ src/main/java/erebus/registries/world/ModStructures.java
@@ -2,10 +2,31 @@ package erebus.registries.world;
 
 import erebus.Erebus;
 import net.minecraft.core.registries.Registries;
+import net.minecraft.data.worldgen.BootstrapContext;
+import net.minecraft.world.level.levelgen.structure.Structure;
+import net.minecraft.world.level.levelgen.structure.StructureSet;
 import net.minecraft.world.level.levelgen.structure.StructureType;
+import net.minecraft.world.level.levelgen.structure.pools.StructureTemplatePool;
+import net.minecraft.world.level.levelgen.structure.templatesystem.StructureProcessorList;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
 public class ModStructures {
 
     public static final DeferredRegister<StructureType<?>> STRUCTURES = DeferredRegister.create(Registries.STRUCTURE_TYPE, Erebus.MODID);
+
+    public static void bootstrapStructures(BootstrapContext<Structure> context) {
+
+    }
+
+    public static void bootstrapSets(BootstrapContext<StructureSet> context) {
+
+    }
+
+    public static void bootstrapPools(BootstrapContext<StructureTemplatePool> context) {
+
+    }
+
+    public static void bootstrapProcessors(BootstrapContext<StructureProcessorList> context) {
+
+    }
 }
