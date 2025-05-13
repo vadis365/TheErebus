106996458b5f15441417008a34b37291d3f1f0f1
vadis365 <6518708+vadis365@users.noreply.github.com>
2025-05-13 12:26:14 +0100

    fixed annoying error

--- src/main/java/erebus/datagen/ModRegistries.java
+++ src/main/java/erebus/datagen/ModRegistries.java
@@ -1,5 +1,8 @@
 package erebus.datagen;
 
+import java.util.Set;
+import java.util.concurrent.CompletableFuture;
+
 import erebus.Erebus;
 import erebus.registries.world.ModBiomes;
 import erebus.registries.world.ModDimensionRegistries;
@@ -11,14 +14,11 @@ import net.minecraft.core.registries.Registries;
 import net.minecraft.data.PackOutput;
 import net.neoforged.neoforge.common.data.DatapackBuiltinEntriesProvider;
 
-import java.util.Set;
-import java.util.concurrent.CompletableFuture;
-
 public class ModRegistries extends DatapackBuiltinEntriesProvider {
 
     public static final RegistrySetBuilder BUILDER = new RegistrySetBuilder()
             .add(Registries.BIOME, ModBiomes::bootstrap)
-            .add(Registries.CONFIGURED_FEATURE, ModFeatures::bootstrapConfiguredFeatures)
+            .add(Registries.CONFIGURED_FEATURE, (T) -> ModFeatures.bootstrapConfiguredFeatures(T))
             .add(Registries.PLACED_FEATURE, ModFeatures::bootstrapPlacedFeatures)
             .add(Registries.DIMENSION_TYPE, ModDimensionRegistries::bootstrapType)
             .add(Registries.NOISE_SETTINGS, ModNoiseGenerator::bootstrap)
