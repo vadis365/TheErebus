559dd09c7bd90108bd6b09aa3359b5e17944d155
vadis365 <6518708+vadis365@users.noreply.github.com>
2025-05-17 16:53:27 +0100

    Liquifier block tidy

--- src/main/java/erebus/block/Liquifier.java
+++ src/main/java/erebus/block/Liquifier.java
@@ -34,11 +34,7 @@ public class Liquifier extends HorizontalDirectionalBlock implements EntityBlock
 
 	public Liquifier(Properties properties) {
 		super(properties);
-		//super(Material.IRON);
 		registerDefaultState(this.stateDefinition.any().setValue(POWERED, false));
-		//setHardness(10.0F);
-		//setSoundType(SoundType.GLASS);
-		//setCreativeTab(ModTabs.BLOCKS);
 	}
 
 	@Override
@@ -74,46 +70,6 @@ public class Liquifier extends HorizontalDirectionalBlock implements EntityBlock
 		builder.add(FACING, POWERED);
 	}
 
-/*
-	@Override
-	public void onBlockHarvested(World world, BlockPos pos, IBlockState state, EntityPlayer player) {
-		if (!world.isRemote && !player.capabilities.isCreativeMode) {
-			TileEntityLiquifier tile = (TileEntityLiquifier) world.getTileEntity(pos);
-			if (tile != null) {
-				NBTTagCompound nbt = new NBTTagCompound();
-				tile.writeToNBT(nbt);
-				ItemStack stack = new ItemStack(Item.getItemFromBlock(this), 1, 0);
-				if (tile.tank.getFluidAmount() > 0)
-					stack.setTagCompound(nbt);
-				InventoryHelper.spawnItemStack(world, pos.getX(), pos.getY(), pos.getZ(), stack);
-				InventoryHelper.dropInventoryItems(world, pos, tile);
-				world.removeTileEntity(pos);
-			}
-		}
-	}
-
-	@Override
-	public void onBlockPlacedBy(World world, BlockPos pos, IBlockState state, EntityLivingBase placer, ItemStack stack) {
-		super.onBlockPlacedBy(world, pos, state, placer, stack);
-		TileEntityLiquifier tile = (TileEntityLiquifier) world.getTileEntity(pos);
-		if (!world.isRemote) {
-			if (stack.hasTagCompound()) {
-				if (tile != null) {
-					if (!stack.getTagCompound().hasKey("Empty")) {
-						FluidStack fluid = FluidStack.loadFluidStackFromNBT(stack.getTagCompound());
-						tile.tank.fillInternal(fluid, true);
-					}
-				}
-			}
-
-			if (state.getValue(POWERED)) {
-				tile.setActive(true);
-			}
-		}
-	}
-
-*/
-
 	@Nonnull
 	@Override
 	public InteractionResult useWithoutItem(BlockState state, @Nonnull Level world, @Nonnull BlockPos pos, @Nonnull Player player, @Nonnull BlockHitResult hitResult) {
@@ -135,7 +91,7 @@ public class Liquifier extends HorizontalDirectionalBlock implements EntityBlock
 			super.onRemove(state, world, pos, newState, isMoving);
 		}
 	}
-	
+
 	@Override
 	public void neighborChanged(BlockState state, Level level, @Nonnull BlockPos pos, @Nonnull Block block, @Nonnull BlockPos fromPos, boolean isMoving) {
 		if (!level.isClientSide()) {
@@ -148,7 +104,7 @@ public class Liquifier extends HorizontalDirectionalBlock implements EntityBlock
 			}
 		}
 	}
-	
+
 	@Override
 	public void tick(@Nonnull BlockState state, ServerLevel world, @Nonnull BlockPos pos, @Nonnull RandomSource rand) {
 		if (!world.isClientSide) {
@@ -157,27 +113,4 @@ public class Liquifier extends HorizontalDirectionalBlock implements EntityBlock
 				world.setBlock(pos, state.setValue(POWERED, flag), 4);
 		}
 	}
-/*
-	@Override
-	public ItemBlock getItemBlock() {
-		ItemBlock LIQUIFIER_ITEM = new ItemBlock(this) {
-			@Override
-			@SideOnly(Side.CLIENT)
-			public void addInformation(ItemStack stack, @Nullable World worldIn, List<String> list, ITooltipFlag flag) {
-				if(stack.hasTagCompound() && !stack.getTagCompound().hasKey("Empty")) {
-					FluidStack fluid = FluidStack.loadFluidStackFromNBT(stack.getTagCompound());
-					if(fluid !=null) {
-						list.add(TextFormatting.GREEN + "Contains: "+ fluid.getFluid().getLocalizedName(fluid));
-						list.add(TextFormatting.BLUE + ""+ fluid.amount +"Mb");
-					}
-				}
-				else {
-					list.add(TextFormatting.YELLOW + new TextComponentTranslation("tooltip.erebus.liquifier").getFormattedText());
-					list.add(TextFormatting.RED + "It's Empty!");
-				}
-			}
-		};
-		return LIQUIFIER_ITEM;
-	}
-	*/
 }
