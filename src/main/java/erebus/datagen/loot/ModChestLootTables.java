3654a142b15b559fa3f5dcb790e88894d4f8ba33
TheCricket <bcord@me.com>
2025-05-21 18:04:06 -0400

    Finish dungeon loot tables

--- src/main/java/erebus/datagen/loot/ModChestLootTables.java
+++ src/main/java/erebus/datagen/loot/ModChestLootTables.java
@@ -40,7 +40,7 @@ public record ModChestLootTables(HolderLookup.Provider registries) implements Lo
     public void generate(BiConsumer<ResourceKey<LootTable>, LootTable.Builder> consumer) {
         consumer.accept(ROTTEN_LOG, LootTable.lootTable()
                 .withPool(LootPool.lootPool()
-                        .setRolls(ConstantValue.exactly(1))
+                        .setRolls(UniformGenerator.between(3, 10))
                         .add(item(Items.BOOK, 18, 2, 4))
                         .add(item(Items.PAPER, 16, 2, 6))
                         .add(item(Items.COBWEB, 13, 2, 7))
@@ -262,13 +262,54 @@ public record ModChestLootTables(HolderLookup.Provider registries) implements Lo
                 .withPool(LootPool.lootPool()
                         .setRolls(UniformGenerator.between(2, 3))
                         .add(item(ModItems.JADE, 10, 1, 3))
+                        .add(item(ModItems.PLATE_EXO, 9, 4, 8))
+                        .add(item(Items.ENCHANTED_BOOK, 8))
+                        .add(item(Items.IRON_PICKAXE, 2))
+                        .add(item(ModItems.JADE_PICKAXE, 1))
+                        .add(item(Items.IRON_AXE, 2))
+                        .add(item(ModItems.JADE_AXE, 1))
+                        .add(item(ModItems.BAMBOO_CHESTPLATE, 2))
+                        .add(item(ModItems.JADE_CHESTPLATE, 1))
+                        .add(item(ModItems.BAMBOO_HELMET, 2))
+                        .add(item(ModItems.JADE_HELMET, 1))
+                        .add(item(ModItems.BAMBOO_LEGGINGS, 2))
+                        .add(item(ModItems.JADE_LEGGINGS, 1))
+                        .add(item(ModItems.BAMBOO_BOOTS, 2))
+                        .add(item(ModItems.EXOSKELETON_BOOTS, 3))
+                        .add(item(ModItems.BAMBOO_SHIELD, 3))
+                        .add(item(ModItems.ANTI_VENOM_BOTTLE, 8, 1, 3))
+                        .add(item(ModItems.SPRAY_CAN, 5, 3, 6))
+                        .add(item(ModItems.FLY_WING, 1))
+                        .add(item(ModItems.REINFORCED_PLATE_EXO, 4, 1, 3))
+                        .add(item(ModItems.BEETLE_TAMING_AMULET, 1))
+                        .add(item(ModItems.BAMBOO_SOUP, 4, 1, 3))
+                        .add(item(ModItems.LARVAE_ON_STICK, 6, 1, 2))
+                        .add(item(ModItems.BAMBUCKET, 3))
+                        .add(item(ModItems.CABBAGE_SEEDS, 5, 1, 3))
+                        .add(item(ModItems.CABBAGE, 10, 1, 3))
+                        .add(item(ModItems.TURNIP, 10, 1, 3))
                 )
         );
 
         consumer.accept(SPIDER_DUNGEON, LootTable.lootTable()
                 .withPool(LootPool.lootPool()
-                        .setRolls(UniformGenerator.between(2, 3))
-                        .add(item(ModItems.JADE, 10, 1, 3))
+                        .setRolls(UniformGenerator.between(3, 10))
+                        .add(item(Items.STRING, 13, 5, 10))
+                        .add(item(ModItems.DARK_FRUIT_PIE, 13, 1, 2))
+                        .add(item(Blocks.COBWEB, 13, 3, 8))
+                        .add(item(Items.STICK, 12, 1, 8))
+                        .add(item(Items.GOLD_NUGGET, 12, 3, 11))
+                        .add(item(ModItems.SHARD_BONE, 12, 3, 8))
+                        .add(item(Items.BONE, 11, 1, 3))
+                        .add(item(Items.IRON_INGOT, 10, 1, 3))
+                        .add(item(Items.GOLD_INGOT, 10, 1, 2))
+                        .add(item(ModItems.FLY_WING, 10, 1, 5))
+                        .add(item(ModItems.JADE, 9))
+                        .add(item(ModItems.PLATE_EXO, 8, 3, 6))
+                        .add(item(ModItems.COMPOUND_EYES, 7, 2, 6))
+                        .add(item(ModItems.COMPOUND_LENS, 2))
+                        .add(item(OtherBlocks.UMBER_GOLEM_STATUE, 1))
+                        .add(item(ModItems.MAX_SPEED_BOW, 1))
                 )
         );
     }
@@ -286,8 +327,7 @@ public record ModChestLootTables(HolderLookup.Provider registries) implements Lo
     }
 
     private LootPoolSingletonContainer.Builder<?> item(ItemLike item, int weight) {
-        return LootItem.lootTableItem(item)
-                .setWeight(weight);
+        return item(item, weight, 1);
     }
 
     private LootPoolSingletonContainer.Builder<?> itemEnchanted(ItemLike item, int weight) {
