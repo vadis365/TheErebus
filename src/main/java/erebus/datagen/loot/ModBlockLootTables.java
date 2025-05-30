abbf13a3c4d1cb094e55e6ea628cef76c86170a5
vadis365 <6518708+vadis365@users.noreply.github.com>
2025-05-30 14:31:59 +0100

    sorted

--- src/main/java/erebus/datagen/loot/ModBlockLootTables.java
+++ src/main/java/erebus/datagen/loot/ModBlockLootTables.java
@@ -275,7 +275,7 @@ public class ModBlockLootTables extends ModBlockLootTableProvider {
         dropSelf(PlantBlocks.HEART_BERRY_BUSH);
         dropSelf(PlantBlocks.SWAMP_BERRY_BUSH);
         dropSelf(PlantBlocks.DARK_FRUIT_VINE);
-        dropPricklyPearBasedOffCondition(PlantBlocks.PRICKLY_PEAR, PlantBlocks.PRICKLY_PEAR, ModItems.PRICKLY_PEAR_RAW);
+        dropPricklyPearBasedOffCondition(PlantBlocks.PRICKLY_PEAR);
         dropSelf(PlantBlocks.DARK_CAPPED_MUSHROOM);
         dropSelf(PlantBlocks.DUTCH_CAP_MUSHROOM);
         dropSelf(PlantBlocks.GRANDMAS_SHOES_MUSHROOM);
--- src/main/java/erebus/datagen/providers/ModBlockLootTableProvider.java
+++ src/main/java/erebus/datagen/providers/ModBlockLootTableProvider.java
@@ -2,8 +2,10 @@ package erebus.datagen.providers;
 
 import erebus.block.PricklyPearBlock;
 import erebus.block.util.ModCropBlock;
+import erebus.registries.ModItems;
 import net.minecraft.advancements.critereon.StatePropertiesPredicate;
 import net.minecraft.core.HolderLookup;
+import net.minecraft.core.component.DataComponents;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.data.loot.BlockLootSubProvider;
 import net.minecraft.world.flag.FeatureFlags;
@@ -11,6 +13,7 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.item.enchantment.Enchantments;
 import net.minecraft.world.level.ItemLike;
+import net.minecraft.world.level.block.BeehiveBlock;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.CropBlock;
 import net.minecraft.world.level.block.SlabBlock;
@@ -18,6 +21,8 @@ import net.minecraft.world.level.storage.loot.LootPool;
 import net.minecraft.world.level.storage.loot.LootTable;
 import net.minecraft.world.level.storage.loot.entries.LootItem;
 import net.minecraft.world.level.storage.loot.functions.ApplyBonusCount;
+import net.minecraft.world.level.storage.loot.functions.CopyBlockState;
+import net.minecraft.world.level.storage.loot.functions.CopyComponentsFunction;
 import net.minecraft.world.level.storage.loot.functions.SetItemCountFunction;
 import net.minecraft.world.level.storage.loot.predicates.LootItemBlockStatePropertyCondition;
 import net.minecraft.world.level.storage.loot.predicates.LootItemCondition;
@@ -74,11 +79,15 @@ public abstract class ModBlockLootTableProvider extends BlockLootSubProvider {
         add(crop.get(), createCropDrops(crop.get(), grownDrop.get(), seed.get(), condition));
     }
 
-    public void dropPricklyPearBasedOffCondition(Supplier<? extends Block> blockIn, Supplier<? extends Block> blockToDrop, Supplier<? extends Item> itemTodrop) {
-        LootItemCondition.Builder condition = LootItemBlockStatePropertyCondition.hasBlockStateProperties(blockIn.get())
-                .setProperties(StatePropertiesPredicate.Builder.properties().hasProperty(PricklyPearBlock.AGE, 11));
-        add(blockIn.get(), createCropDrops(blockIn.get(), itemTodrop.get(), blockToDrop.get().asItem(), condition));
-    }
+	public void dropPricklyPearBasedOffCondition(Supplier<? extends Block> blockIn) {
+		LootItemCondition.Builder condition = LootItemBlockStatePropertyCondition.hasBlockStateProperties(blockIn.get())
+				.setProperties(StatePropertiesPredicate.Builder.properties().hasProperty(PricklyPearBlock.AGE, 11));
+		add(blockIn.get(),
+				LootTable.lootTable()
+						.withPool(LootPool.lootPool().setRolls(ConstantValue.exactly(1.0F))
+								.add(LootItem.lootTableItem(ModItems.PRICKLY_PEAR_RAW).when(condition)
+										.otherwise(LootItem.lootTableItem(blockIn.get().asItem())))));
+	}
 
     public void dropComponents(Supplier<? extends Block> blockSupplier, Consumer<LootPool.Builder> lootFunctionSupplier) {
         LootPool.Builder lootPool = LootPool.lootPool().setRolls(ConstantValue.exactly(1))
