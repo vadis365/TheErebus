cbfa2b4d28772253241c588a45464c2d564a9959
vadis365 <6518708+vadis365@users.noreply.github.com>
2025-05-31 16:40:27 +0100

    bamboo shearing \o/

--- src/main/java/erebus/block/BambooBlock.java
+++ src/main/java/erebus/block/BambooBlock.java
@@ -1,25 +1,42 @@
 package erebus.block;
 
+import java.util.Optional;
+
+import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
 import org.jetbrains.annotations.NotNull;
 
+import erebus.block.entity.FluidJarBlockEntity;
+import erebus.registries.blocks.providers.WoodBlocks;
+import erebus.utils.CapHelper;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.util.RandomSource;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.ItemInteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.IntegerProperty;
+import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
 import net.neoforged.neoforge.common.util.TriState;
+import net.neoforged.neoforge.fluids.FluidUtil;
+import net.neoforged.neoforge.fluids.capability.IFluidHandler;
 
 public class BambooBlock extends Block {
 
@@ -92,6 +109,22 @@ public class BambooBlock extends Block {
 			}
 
 		}
+	}
+
+	@Nonnull
+	@Override
+	public ItemInteractionResult useItemOn(ItemStack stack, BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult hit) {
+		if (level.isClientSide)
+			return ItemInteractionResult.SUCCESS;
+
+		if (stack.is(Items.SHEARS)) {
+			if (state.getValue(AGE) == 15) {
+				level.setBlock(pos, state.setValue(AGE, 0), 4);
+				popResource(level, pos, new ItemStack(WoodBlocks.SAPLING_BAMBOO.asItem()));
+				return ItemInteractionResult.SUCCESS;
+			}
+		}
+		return ItemInteractionResult.PASS_TO_DEFAULT_BLOCK_INTERACTION;
 	}
 
 	@Override
