f60ed4d07f2e0742612731f7b7d98d6c8ce137ed
Brandon Cord <bcord@me.com>
2026-02-10 12:34:05 -0500

    Add rendering state management for Centipede entity

--- src/main/java/erebus/client/render/entity/renderer/state/CentipedeRenderState.java
+++ src/main/java/erebus/client/render/entity/renderer/state/CentipedeRenderState.java
@@ -4,4 +4,16 @@ import net.minecraft.client.renderer.entity.state.LivingEntityRenderState;
 
 public class CentipedeRenderState extends LivingEntityRenderState {
     public int skin;
+    public float avgWibbleStrength;
+    public PartState head;
+    public PartState[] bodyParts;
+    public PartState tail;
+
+    public static class PartState {
+        public double x, y, z;
+        public float yaw;
+        public float wibbleStrength;
+        public int frame;
+        public boolean isPartA;
+    }
 }
